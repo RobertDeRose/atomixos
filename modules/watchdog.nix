RobertDeRose/AtomicNIx

feat(modules): add durable log batching
Move Layer 2 to volatile journald feeding buffered rsyslog appends so general diagnostics stay
memory-first without the extra runtime flush timer.

Record slot transitions and watchdog rollback markers on the real path and cover the behavior with
focused forensics tests.

modules/watchdog.nix
@@ -5,9 +5,18 @@
   config,
   lib,
   pkgs,
+  self,
   ...
 }:
 
+let
+  forensicCli = pkgs.writeShellScriptBin "forensic-log" (
+    builtins.readFile ../scripts/forensic-log.sh
+  );
+  watchdogBootCountCli = pkgs.writeShellScriptBin "watchdog-boot-count" (
+    builtins.readFile ../scripts/watchdog-boot-count.sh
+  );
+in
 {
   # ── Watchdog ─────────────────────────────────────────────────────────────────
 
@@ -19,4 +28,31 @@
   # RuntimeWatchdogSec = "30s";
   # RebootWatchdogSec = "10min";
   systemd.settings.Manager = { };
+
+  environment.systemPackages = [
+    forensicCli
+    watchdogBootCountCli
+  ];
+
+  systemd.services.watchdog-boot-count = {
+    description = "Record watchdog boot-count and rollback state";
+    wantedBy = [ "multi-user.target" ];
+    before = [ "rauc.service" ];
+    after = [ "local-fs.target" ];
+    path = [
+      forensicCli
+      watchdogBootCountCli
+    ]
+    ++ lib.optionals (config.atomicnix.rauc.bootloader == "uboot") [
+      self.packages.${pkgs.stdenv.hostPlatform.system}.uboot-env-tools
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${watchdogBootCountCli}/bin/watchdog-boot-count";
+    };
+    environment = {
+      ATOMICNIX_RAUC_BOOTLOADER = config.atomicnix.rauc.bootloader;
+      ATOMICNIX_RAUC_STATE_DIR = "/var/lib/rauc";
+    };
+  };
 }

nix/tests/forensics-podman-log-path.nix
@@ -1,0 +1,81 @@
+{
+  pkgs,
+  hostPkgs ? pkgs,
+  qemuModule,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+  testImage = pkgs.dockerTools.buildImage {
+    name = "podman-log-test";
+    tag = "latest";
+    copyToRoot = pkgs.buildEnv {
+      name = "podman-log-test-root";
+      paths = [ pkgs.busybox ];
+      pathsToLink = [ "/bin" ];
+    };
+    config = {
+      Cmd = [
+        "/bin/sh"
+        "-c"
+        "echo podman-log-check"
+      ];
+    };
+  };
+in
+nixos-lib.runTest {
+  name = "forensics-podman-log-path";
+
+  inherit hostPkgs;
+
+  nodes.machine =
+    { lib, ... }:
+    {
+      imports = [
+        ../../modules/rauc.nix
+        ../../modules/forensics.nix
+        qemuModule
+      ];
+
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+
+      environment.etc."atomicnix/current-boot-forensics-mount".source = lib.mkForce (
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          printf '%s\n' /boot
+        ''
+      );
+
+      virtualisation.podman = {
+        enable = true;
+        dockerCompat = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+
+      virtualisation.containers.storage.settings = {
+        storage = {
+          graphroot = "/data/containers/storage";
+          runroot = "/run/containers/storage";
+        };
+      };
+
+      systemd.tmpfiles.rules = [ "d /boot/forensics 0755 root root -" ];
+    };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+    machine.wait_for_unit("syslog.service")
+
+    machine.succeed("grep 'log_driver = \"journald\"' /etc/containers/containers.conf")
+
+    machine.copy_from_host("${testImage}", "/tmp/podman-log-test.tar.gz")
+    machine.succeed("podman load -i /tmp/podman-log-test.tar.gz")
+    machine.succeed("podman run --rm --network=none --name podman-log-test localhost/podman-log-test:latest")
+
+    machine.wait_until_succeeds("journalctl --since '1 minute ago' -o cat | grep '^podman-log-check$'")
+
+    machine.succeed("systemctl kill -s HUP syslog.service")
+    machine.wait_until_succeeds("grep 'podman-log-check' /data/logs/messages.log")
+  '';
+}

nix/tests/forensics-shutdown-flush.nix
@@ -1,0 +1,50 @@
+{
+  pkgs,
+  hostPkgs ? pkgs,
+  qemuModule,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
+nixos-lib.runTest {
+  name = "forensics-shutdown-flush";
+
+  inherit hostPkgs;
+
+  nodes.machine =
+    { lib, ... }:
+    {
+      imports = [
+        ../../modules/rauc.nix
+        ../../modules/forensics.nix
+        qemuModule
+      ];
+
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+
+      environment.etc."atomicnix/current-boot-forensics-mount".source = lib.mkForce (
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          printf '%s\n' /boot
+        ''
+      );
+
+      environment.systemPackages = [ pkgs.logger ];
+
+      systemd.tmpfiles.rules = [ "d /boot/forensics 0755 root root -" ];
+    };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+    machine.wait_for_unit("syslog.service")
+
+    machine.succeed("logger -t forensics-shutdown-flush 'shutdown-flush-check'")
+    machine.succeed("systemctl start forensics-shutdown-flush.service")
+
+    machine.wait_until_succeeds("grep 'shutdown-flush-check' /data/logs/messages.log")
+    machine.succeed("grep 'slot=boot.0 stage=shutdown event=flush-begin result=start' /boot/forensics/segment-0.log")
+    machine.succeed("grep 'slot=boot.0 stage=shutdown event=flush-end result=ok' /boot/forensics/segment-0.log")
+  '';
+}

nix/tests/forensics-slot-transition.nix
@@ -1,0 +1,59 @@
+{
+  pkgs,
+  hostPkgs ? pkgs,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
+nixos-lib.runTest {
+  name = "forensics-slot-transition";
+
+  inherit hostPkgs;
+
+  nodes.machine =
+    { ... }:
+    {
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+      environment.etc."atomicnix/current-boot-forensics-mount".source =
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          case "''${1:-boot.0}" in
+            boot.0) printf '%s\n' /tmp/forensics/boot.0 ;;
+            boot.1) printf '%s\n' /tmp/forensics/boot.1 ;;
+            *) printf '%s\n' /tmp/forensics/boot.0 ;;
+          esac
+        '';
+      environment.systemPackages = [
+        (pkgs.writeShellScriptBin "forensic-log" (builtins.readFile ../../scripts/forensic-log.sh))
+        (pkgs.writeShellScriptBin "forensics-slot-transition" (
+          builtins.readFile ../../scripts/forensics-slot-transition.sh
+        ))
+      ];
+      systemd.tmpfiles.rules = [
+        "d /tmp/forensics 0755 root root -"
+        "d /tmp/forensics/boot.0 0755 root root -"
+        "d /tmp/forensics/boot.1 0755 root root -"
+        "d /data 0755 root root -"
+        "d /data/rauc 0755 root root -"
+        "d /data/rauc/forensics 0755 root root -"
+      ];
+    };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+
+    machine.succeed("printf 'boot.0\n' > /data/rauc/forensics/pending-source-slot")
+    machine.succeed("printf 'boot.1\n' > /data/rauc/forensics/pending-target-slot")
+    machine.succeed("printf '2.0.0\n' > /data/rauc/forensics/pending-target-version")
+
+    machine.succeed("ATOMICNIX_FORENSICS_SLOT=boot.1 forensics-slot-transition")
+    machine.succeed("grep 'slot=boot.1 stage=rauc event=slot-switch result=ok target_slot=boot.1 version=2.0.0' /tmp/forensics/boot.1/forensics/segment-0.log")
+
+    machine.succeed("ATOMICNIX_FORENSICS_SLOT=boot.0 forensics-slot-transition")
+    machine.succeed("grep 'slot=boot.1 stage=rollback event=detected target_slot=boot.0 reason=slot-fallback version=2.0.0' /tmp/forensics/boot.1/forensics/segment-0.log")
+    machine.succeed("grep 'slot=boot.1 stage=rollback event=slot-fallback result=ok target_slot=boot.0 version=2.0.0' /tmp/forensics/boot.1/forensics/segment-0.log")
+    machine.fail("test -e /data/rauc/forensics/pending-target-slot")
+  '';
+}

scripts/first-boot.sh
@@ -8,6 +8,7 @@
 set -euo pipefail
 
 log() { echo "[first-boot] $*"; }
+FORENSICS_STATE_DIR="${ATOMICNIX_FORENSICS_RAUC_STATE_DIR:-/data/rauc/forensics}"
 
 forensic() {
 	if command -v forensic-log >/dev/null 2>&1; then
@@ -84,6 +85,8 @@ fi
 
 forensic --stage firstboot --event start --slot "$BOOT_SLOT"
 
+mkdir -p "$FORENSICS_STATE_DIR"
+
 ensure_rauc_env
 
 log "Marking current slot as good via RAUC: $BOOT_SLOT"
@@ -92,6 +95,11 @@ if ! rauc status mark-good "$BOOT_SLOT"; then
 	exit 1
 fi
 forensic --stage rauc --event mark-good-complete --slot "$BOOT_SLOT" --result ok
+rm -f \
+	"$FORENSICS_STATE_DIR/pending-source-slot" \
+	"$FORENSICS_STATE_DIR/pending-target-slot" \
+	"$FORENSICS_STATE_DIR/pending-target-version" \
+	"$FORENSICS_STATE_DIR/pending-target-booted"
 
 write_dev_admin_password_hash
 enable_dev_ssh_on_wan

scripts/forensics-slot-transition.sh
@@ -1,0 +1,72 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+STATE_DIR="${ATOMICNIX_FORENSICS_RAUC_STATE_DIR:-/data/rauc/forensics}"
+SOURCE_FILE="$STATE_DIR/pending-source-slot"
+TARGET_FILE="$STATE_DIR/pending-target-slot"
+VERSION_FILE="$STATE_DIR/pending-target-version"
+BOOTED_FILE="$STATE_DIR/pending-target-booted"
+
+current_boot_slot() {
+	local arg
+	for arg in $(</proc/cmdline); do
+		case "$arg" in
+		rauc.slot=boot.*)
+			printf '%s\n' "${arg#rauc.slot=}"
+			return 0
+			;;
+		esac
+	done
+	return 1
+}
+
+read_optional() {
+	local path="$1"
+	if [ -f "$path" ]; then
+		tr -d '\n' <"$path"
+	fi
+}
+
+SOURCE_SLOT="$(read_optional "$SOURCE_FILE")"
+TARGET_SLOT="$(read_optional "$TARGET_FILE")"
+TARGET_VERSION="$(read_optional "$VERSION_FILE")"
+CURRENT_SLOT="${ATOMICNIX_FORENSICS_SLOT:-$(current_boot_slot || true)}"
+
+if [ -z "$SOURCE_SLOT" ] || [ -z "$TARGET_SLOT" ] || [ -z "$CURRENT_SLOT" ]; then
+	exit 0
+fi
+
+version_args=()
+if [ -n "$TARGET_VERSION" ]; then
+	version_args=(--version "$TARGET_VERSION")
+fi
+
+if [ "$CURRENT_SLOT" = "$TARGET_SLOT" ] && [ ! -f "$BOOTED_FILE" ]; then
+	forensic-log \
+		--stage rauc \
+		--event slot-switch \
+		--slot "$CURRENT_SLOT" \
+		--target-slot "$TARGET_SLOT" \
+		--result ok \
+		"${version_args[@]}"
+	: >"$BOOTED_FILE"
+	exit 0
+fi
+
+if [ "$CURRENT_SLOT" = "$SOURCE_SLOT" ] && [ -f "$BOOTED_FILE" ]; then
+	forensic-log \
+		--stage rollback \
+		--event detected \
+		--slot "$TARGET_SLOT" \
+		--target-slot "$SOURCE_SLOT" \
+		--reason slot-fallback \
+		"${version_args[@]}"
+	forensic-log \
+		--stage rollback \
+		--event slot-fallback \
+		--slot "$TARGET_SLOT" \
+		--target-slot "$SOURCE_SLOT" \
+		--result ok \
+		"${version_args[@]}"
+	rm -f "$SOURCE_FILE" "$TARGET_FILE" "$VERSION_FILE" "$BOOTED_FILE"
+fi

scripts/os-upgrade.sh
@@ -11,6 +11,7 @@ set -euo pipefail
 UPDATE_URL="${OS_UPGRADE_URL:-http://localhost/updates}"
 DEVICE_ID=$(cat /sys/class/net/eth0/address 2>/dev/null | tr -d ':' || echo "unknown")
 BUNDLE_DIR="/data/config/bundles"
+FORENSICS_STATE_DIR="${ATOMICNIX_FORENSICS_RAUC_STATE_DIR:-/data/rauc/forensics}"
 CURRENT_VERSION=$(rauc status --output-format=json 2>/dev/null | jq -r '
   .booted as $booted
   | .slots[]
@@ -25,6 +26,41 @@ forensic() {
 	if command -v forensic-log >/dev/null 2>&1; then
 		forensic-log "$@" || true
 	fi
+}
+
+current_boot_slot() {
+	local arg
+	for arg in $(</proc/cmdline); do
+		case "$arg" in
+		rauc.slot=boot.*)
+			printf '%s\n' "${arg#rauc.slot=}"
+			return 0
+			;;
+		esac
+	done
+	return 1
+}
+
+slot_letter_to_boot_slot() {
+	case "$1" in
+	A) printf 'boot.0\n' ;;
+	B) printf 'boot.1\n' ;;
+	*) return 1 ;;
+	esac
+}
+
+target_boot_slot_from_status() {
+	local current_letter target_letter
+	current_letter="$(rauc status --output-format=json 2>/dev/null | jq -r '.booted // empty' 2>/dev/null || true)"
+	if [ -z "$current_letter" ]; then
+		return 1
+	fi
+	case "$current_letter" in
+	A) target_letter="B" ;;
+	B) target_letter="A" ;;
+	*) return 1 ;;
+	esac
+	slot_letter_to_boot_slot "$target_letter"
 }
 
 log "Checking for updates (current version: $CURRENT_VERSION, device: $DEVICE_ID)..."
@@ -52,7 +88,12 @@ if [ "$LATEST_VERSION" = "$CURRENT_VERSION" ]; then
 fi
 
 log "New version available: $LATEST_VERSION (current: $CURRENT_VERSION)"
-forensic --stage rauc --event install-start --target-slot unknown --version "$LATEST_VERSION"
+BOOT_SLOT="$(current_boot_slot || true)"
+TARGET_BOOT_SLOT="$(target_boot_slot_from_status || true)"
+if [ -z "$TARGET_BOOT_SLOT" ]; then
+	TARGET_BOOT_SLOT="unknown"
+fi
+forensic --stage rauc --event install-start --slot "$BOOT_SLOT" --target-slot "$TARGET_BOOT_SLOT" --version "$LATEST_VERSION"
 
 # Download the bundle
 mkdir -p "$BUNDLE_DIR"
@@ -73,7 +114,18 @@ log "Download complete"
 log "Installing bundle..."
 if rauc install "$BUNDLE_PATH"; then
 	log "Bundle installed successfully, cleaning up"
-	forensic --stage rauc --event install-complete --version "$LATEST_VERSION" --result ok
+	mkdir -p "$FORENSICS_STATE_DIR"
+	if [ -n "$BOOT_SLOT" ]; then
+		printf '%s\n' "$BOOT_SLOT" >"$FORENSICS_STATE_DIR/pending-source-slot"
+	fi
+	if [ "$TARGET_BOOT_SLOT" != "unknown" ]; then
+		printf '%s\n' "$TARGET_BOOT_SLOT" >"$FORENSICS_STATE_DIR/pending-target-slot"
+	else
+		rm -f "$FORENSICS_STATE_DIR/pending-target-slot"
+	fi
+	printf '%s\n' "$LATEST_VERSION" >"$FORENSICS_STATE_DIR/pending-target-version"
+	rm -f "$FORENSICS_STATE_DIR/pending-target-booted"
+	forensic --stage rauc --event install-complete --slot "$TARGET_BOOT_SLOT" --target-slot "$TARGET_BOOT_SLOT" --version "$LATEST_VERSION" --result ok
 	rm -f "$BUNDLE_PATH"
 	log "Rebooting into new slot..."
 	forensic --stage shutdown --event reboot-requested --result ok --detail os-upgrade

RobertDeRose/atomixos

modules/watchdog.nix
@@ -5,9 +5,18 @@
   config,
   lib,
   pkgs,
+  self,
   ...
 }:
 
+let
+  forensicCli = pkgs.writeShellScriptBin "forensic-log" (
+    builtins.readFile ../scripts/forensic-log.sh
+  );
+  watchdogBootCountCli = pkgs.writeShellScriptBin "watchdog-boot-count" (
+    builtins.readFile ../scripts/watchdog-boot-count.sh
+  );
+in
 {
   # ── Watchdog ─────────────────────────────────────────────────────────────────
 
@@ -19,4 +28,31 @@
   # RuntimeWatchdogSec = "30s";
   # RebootWatchdogSec = "10min";
   systemd.settings.Manager = { };
+
+  environment.systemPackages = [
+    forensicCli
+    watchdogBootCountCli
+  ];
+
+  systemd.services.watchdog-boot-count = {
+    description = "Record watchdog boot-count and rollback state";
+    wantedBy = [ "multi-user.target" ];
+    before = [ "rauc.service" ];
+    after = [ "local-fs.target" ];
+    path = [
+      forensicCli
+      watchdogBootCountCli
+    ]
+    ++ lib.optionals (config.atomixos.rauc.bootloader == "uboot") [
+      self.packages.${pkgs.stdenv.hostPlatform.system}.uboot-env-tools
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${watchdogBootCountCli}/bin/watchdog-boot-count";
+    };
+    environment = {
+      ATOMIXOS_RAUC_BOOTLOADER = config.atomixos.rauc.bootloader;
+      ATOMIXOS_RAUC_STATE_DIR = "/var/lib/rauc";
+    };
+  };
 }

nix/tests/forensics-podman-log-path.nix
@@ -1,0 +1,81 @@
+{
+  pkgs,
+  hostPkgs ? pkgs,
+  qemuModule,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+  testImage = pkgs.dockerTools.buildImage {
+    name = "podman-log-test";
+    tag = "latest";
+    copyToRoot = pkgs.buildEnv {
+      name = "podman-log-test-root";
+      paths = [ pkgs.busybox ];
+      pathsToLink = [ "/bin" ];
+    };
+    config = {
+      Cmd = [
+        "/bin/sh"
+        "-c"
+        "echo podman-log-check"
+      ];
+    };
+  };
+in
+nixos-lib.runTest {
+  name = "forensics-podman-log-path";
+
+  inherit hostPkgs;
+
+  nodes.machine =
+    { lib, ... }:
+    {
+      imports = [
+        ../../modules/rauc.nix
+        ../../modules/forensics.nix
+        qemuModule
+      ];
+
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+
+      environment.etc."atomixos/current-boot-forensics-mount".source = lib.mkForce (
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          printf '%s\n' /boot
+        ''
+      );
+
+      virtualisation.podman = {
+        enable = true;
+        dockerCompat = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+
+      virtualisation.containers.storage.settings = {
+        storage = {
+          graphroot = "/data/containers/storage";
+          runroot = "/run/containers/storage";
+        };
+      };
+
+      systemd.tmpfiles.rules = [ "d /boot/forensics 0755 root root -" ];
+    };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+    machine.wait_for_unit("syslog.service")
+
+    machine.succeed("grep 'log_driver = \"journald\"' /etc/containers/containers.conf")
+
+    machine.copy_from_host("${testImage}", "/tmp/podman-log-test.tar.gz")
+    machine.succeed("podman load -i /tmp/podman-log-test.tar.gz")
+    machine.succeed("podman run --rm --network=none --name podman-log-test localhost/podman-log-test:latest")
+
+    machine.wait_until_succeeds("journalctl --since '1 minute ago' -o cat | grep '^podman-log-check$'")
+
+    machine.succeed("systemctl kill -s HUP syslog.service")
+    machine.wait_until_succeeds("grep 'podman-log-check' /data/logs/messages.log")
+  '';
+}

nix/tests/forensics-shutdown-flush.nix
@@ -1,0 +1,50 @@
+{
+  pkgs,
+  hostPkgs ? pkgs,
+  qemuModule,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
+nixos-lib.runTest {
+  name = "forensics-shutdown-flush";
+
+  inherit hostPkgs;
+
+  nodes.machine =
+    { lib, ... }:
+    {
+      imports = [
+        ../../modules/rauc.nix
+        ../../modules/forensics.nix
+        qemuModule
+      ];
+
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+
+      environment.etc."atomixos/current-boot-forensics-mount".source = lib.mkForce (
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          printf '%s\n' /boot
+        ''
+      );
+
+      environment.systemPackages = [ pkgs.logger ];
+
+      systemd.tmpfiles.rules = [ "d /boot/forensics 0755 root root -" ];
+    };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+    machine.wait_for_unit("syslog.service")
+
+    machine.succeed("logger -t forensics-shutdown-flush 'shutdown-flush-check'")
+    machine.succeed("systemctl start forensics-shutdown-flush.service")
+
+    machine.wait_until_succeeds("grep 'shutdown-flush-check' /data/logs/messages.log")
+    machine.succeed("grep 'slot=boot.0 stage=shutdown event=flush-begin result=start' /boot/forensics/segment-0.log")
+    machine.succeed("grep 'slot=boot.0 stage=shutdown event=flush-end result=ok' /boot/forensics/segment-0.log")
+  '';
+}

nix/tests/forensics-slot-transition.nix
@@ -1,0 +1,59 @@
+{
+  pkgs,
+  hostPkgs ? pkgs,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
+nixos-lib.runTest {
+  name = "forensics-slot-transition";
+
+  inherit hostPkgs;
+
+  nodes.machine =
+    { ... }:
+    {
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+      environment.etc."atomixos/current-boot-forensics-mount".source =
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          case "''${1:-boot.0}" in
+            boot.0) printf '%s\n' /tmp/forensics/boot.0 ;;
+            boot.1) printf '%s\n' /tmp/forensics/boot.1 ;;
+            *) printf '%s\n' /tmp/forensics/boot.0 ;;
+          esac
+        '';
+      environment.systemPackages = [
+        (pkgs.writeShellScriptBin "forensic-log" (builtins.readFile ../../scripts/forensic-log.sh))
+        (pkgs.writeShellScriptBin "forensics-slot-transition" (
+          builtins.readFile ../../scripts/forensics-slot-transition.sh
+        ))
+      ];
+      systemd.tmpfiles.rules = [
+        "d /tmp/forensics 0755 root root -"
+        "d /tmp/forensics/boot.0 0755 root root -"
+        "d /tmp/forensics/boot.1 0755 root root -"
+        "d /data 0755 root root -"
+        "d /data/rauc 0755 root root -"
+        "d /data/rauc/forensics 0755 root root -"
+      ];
+    };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+
+    machine.succeed("printf 'boot.0\n' > /data/rauc/forensics/pending-source-slot")
+    machine.succeed("printf 'boot.1\n' > /data/rauc/forensics/pending-target-slot")
+    machine.succeed("printf '2.0.0\n' > /data/rauc/forensics/pending-target-version")
+
+    machine.succeed("ATOMIXOS_FORENSICS_SLOT=boot.1 forensics-slot-transition")
+    machine.succeed("grep 'slot=boot.1 stage=rauc event=slot-switch result=ok target_slot=boot.1 version=2.0.0' /tmp/forensics/boot.1/forensics/segment-0.log")
+
+    machine.succeed("ATOMIXOS_FORENSICS_SLOT=boot.0 forensics-slot-transition")
+    machine.succeed("grep 'slot=boot.1 stage=rollback event=detected target_slot=boot.0 reason=slot-fallback version=2.0.0' /tmp/forensics/boot.1/forensics/segment-0.log")
+    machine.succeed("grep 'slot=boot.1 stage=rollback event=slot-fallback result=ok target_slot=boot.0 version=2.0.0' /tmp/forensics/boot.1/forensics/segment-0.log")
+    machine.fail("test -e /data/rauc/forensics/pending-target-slot")
+  '';
+}

scripts/first-boot.sh
@@ -8,6 +8,7 @@
 set -euo pipefail
 
 log() { echo "[first-boot] $*"; }
+FORENSICS_STATE_DIR="${ATOMIXOS_FORENSICS_RAUC_STATE_DIR:-/data/rauc/forensics}"
 
 forensic() {
 	if command -v forensic-log >/dev/null 2>&1; then
@@ -84,6 +85,8 @@ fi
 
 forensic --stage firstboot --event start --slot "$BOOT_SLOT"
 
+mkdir -p "$FORENSICS_STATE_DIR"
+
 ensure_rauc_env
 
 log "Marking current slot as good via RAUC: $BOOT_SLOT"
@@ -92,6 +95,11 @@ if ! rauc status mark-good "$BOOT_SLOT"; then
 	exit 1
 fi
 forensic --stage rauc --event mark-good-complete --slot "$BOOT_SLOT" --result ok
+rm -f \
+	"$FORENSICS_STATE_DIR/pending-source-slot" \
+	"$FORENSICS_STATE_DIR/pending-target-slot" \
+	"$FORENSICS_STATE_DIR/pending-target-version" \
+	"$FORENSICS_STATE_DIR/pending-target-booted"
 
 write_dev_admin_password_hash
 enable_dev_ssh_on_wan

scripts/forensics-slot-transition.sh
@@ -1,0 +1,72 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+STATE_DIR="${ATOMIXOS_FORENSICS_RAUC_STATE_DIR:-/data/rauc/forensics}"
+SOURCE_FILE="$STATE_DIR/pending-source-slot"
+TARGET_FILE="$STATE_DIR/pending-target-slot"
+VERSION_FILE="$STATE_DIR/pending-target-version"
+BOOTED_FILE="$STATE_DIR/pending-target-booted"
+
+current_boot_slot() {
+	local arg
+	for arg in $(</proc/cmdline); do
+		case "$arg" in
+		rauc.slot=boot.*)
+			printf '%s\n' "${arg#rauc.slot=}"
+			return 0
+			;;
+		esac
+	done
+	return 1
+}
+
+read_optional() {
+	local path="$1"
+	if [ -f "$path" ]; then
+		tr -d '\n' <"$path"
+	fi
+}
+
+SOURCE_SLOT="$(read_optional "$SOURCE_FILE")"
+TARGET_SLOT="$(read_optional "$TARGET_FILE")"
+TARGET_VERSION="$(read_optional "$VERSION_FILE")"
+CURRENT_SLOT="${ATOMIXOS_FORENSICS_SLOT:-$(current_boot_slot || true)}"
+
+if [ -z "$SOURCE_SLOT" ] || [ -z "$TARGET_SLOT" ] || [ -z "$CURRENT_SLOT" ]; then
+	exit 0
+fi
+
+version_args=()
+if [ -n "$TARGET_VERSION" ]; then
+	version_args=(--version "$TARGET_VERSION")
+fi
+
+if [ "$CURRENT_SLOT" = "$TARGET_SLOT" ] && [ ! -f "$BOOTED_FILE" ]; then
+	forensic-log \
+		--stage rauc \
+		--event slot-switch \
+		--slot "$CURRENT_SLOT" \
+		--target-slot "$TARGET_SLOT" \
+		--result ok \
+		"${version_args[@]}"
+	: >"$BOOTED_FILE"
+	exit 0
+fi
+
+if [ "$CURRENT_SLOT" = "$SOURCE_SLOT" ] && [ -f "$BOOTED_FILE" ]; then
+	forensic-log \
+		--stage rollback \
+		--event detected \
+		--slot "$TARGET_SLOT" \
+		--target-slot "$SOURCE_SLOT" \
+		--reason slot-fallback \
+		"${version_args[@]}"
+	forensic-log \
+		--stage rollback \
+		--event slot-fallback \
+		--slot "$TARGET_SLOT" \
+		--target-slot "$SOURCE_SLOT" \
+		--result ok \
+		"${version_args[@]}"
+	rm -f "$SOURCE_FILE" "$TARGET_FILE" "$VERSION_FILE" "$BOOTED_FILE"
+fi

scripts/os-upgrade.sh
@@ -11,6 +11,7 @@ set -euo pipefail
 UPDATE_URL="${OS_UPGRADE_URL:-http://localhost/updates}"
 DEVICE_ID=$(cat /sys/class/net/eth0/address 2>/dev/null | tr -d ':' || echo "unknown")
 BUNDLE_DIR="/data/config/bundles"
+FORENSICS_STATE_DIR="${ATOMIXOS_FORENSICS_RAUC_STATE_DIR:-/data/rauc/forensics}"
 CURRENT_VERSION=$(rauc status --output-format=json 2>/dev/null | jq -r '
   .booted as $booted
   | .slots[]
@@ -25,6 +26,41 @@ forensic() {
 	if command -v forensic-log >/dev/null 2>&1; then
 		forensic-log "$@" || true
 	fi
+}
+
+current_boot_slot() {
+	local arg
+	for arg in $(</proc/cmdline); do
+		case "$arg" in
+		rauc.slot=boot.*)
+			printf '%s\n' "${arg#rauc.slot=}"
+			return 0
+			;;
+		esac
+	done
+	return 1
+}
+
+slot_letter_to_boot_slot() {
+	case "$1" in
+	A) printf 'boot.0\n' ;;
+	B) printf 'boot.1\n' ;;
+	*) return 1 ;;
+	esac
+}
+
+target_boot_slot_from_status() {
+	local current_letter target_letter
+	current_letter="$(rauc status --output-format=json 2>/dev/null | jq -r '.booted // empty' 2>/dev/null || true)"
+	if [ -z "$current_letter" ]; then
+		return 1
+	fi
+	case "$current_letter" in
+	A) target_letter="B" ;;
+	B) target_letter="A" ;;
+	*) return 1 ;;
+	esac
+	slot_letter_to_boot_slot "$target_letter"
 }
 
 log "Checking for updates (current version: $CURRENT_VERSION, device: $DEVICE_ID)..."
@@ -52,7 +88,12 @@ if [ "$LATEST_VERSION" = "$CURRENT_VERSION" ]; then
 fi
 
 log "New version available: $LATEST_VERSION (current: $CURRENT_VERSION)"
-forensic --stage rauc --event install-start --target-slot unknown --version "$LATEST_VERSION"
+BOOT_SLOT="$(current_boot_slot || true)"
+TARGET_BOOT_SLOT="$(target_boot_slot_from_status || true)"
+if [ -z "$TARGET_BOOT_SLOT" ]; then
+	TARGET_BOOT_SLOT="unknown"
+fi
+forensic --stage rauc --event install-start --slot "$BOOT_SLOT" --target-slot "$TARGET_BOOT_SLOT" --version "$LATEST_VERSION"
 
 # Download the bundle
 mkdir -p "$BUNDLE_DIR"
@@ -73,7 +114,18 @@ log "Download complete"
 log "Installing bundle..."
 if rauc install "$BUNDLE_PATH"; then
 	log "Bundle installed successfully, cleaning up"
-	forensic --stage rauc --event install-complete --version "$LATEST_VERSION" --result ok
+	mkdir -p "$FORENSICS_STATE_DIR"
+	if [ -n "$BOOT_SLOT" ]; then
+		printf '%s\n' "$BOOT_SLOT" >"$FORENSICS_STATE_DIR/pending-source-slot"
+	fi
+	if [ "$TARGET_BOOT_SLOT" != "unknown" ]; then
+		printf '%s\n' "$TARGET_BOOT_SLOT" >"$FORENSICS_STATE_DIR/pending-target-slot"
+	else
+		rm -f "$FORENSICS_STATE_DIR/pending-target-slot"
+	fi
+	printf '%s\n' "$LATEST_VERSION" >"$FORENSICS_STATE_DIR/pending-target-version"
+	rm -f "$FORENSICS_STATE_DIR/pending-target-booted"
+	forensic --stage rauc --event install-complete --slot "$TARGET_BOOT_SLOT" --target-slot "$TARGET_BOOT_SLOT" --version "$LATEST_VERSION" --result ok
 	rm -f "$BUNDLE_PATH"
 	log "Rebooting into new slot..."
 	forensic --stage shutdown --event reboot-requested --result ok --detail os-upgrade