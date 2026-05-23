RobertDeRose/AtomicNIx

feat(tests): RAUC slot logic NixOS test (task 15.5)
Refactor rauc.nix to use configurable slot device paths via NixOS
options (atomicnix.rauc.slots), allowing hardware modules to provide
their own paths — eMMC for Rock64, virtio for QEMU.

Add nix/tests/rauc-slots.nix: a minimal NixOS VM test that boots with
four extra virtio-blk disks and verifies RAUC recognizes all A/B slot
pairs. The test starts a RAUC D-Bus service and confirms rauc status
reports boot.0/1 and rootfs.0/1 with the correct device paths.

Key fixes to system.conf during development:
- Removed invalid [handlers] section (RAUC rejected it)
- Removed duplicate bootname on rootfs slots (must be unique per slot)
- Added rauc.slot=boot.0 kernel param for slot identification
- Override status file path for test VM (/tmp vs /persist)

modules/hardware-qemu.nix
@@ -33,6 +33,19 @@
   # (systemd watchdog config from watchdog.nix still applies,
   #  but hardware watchdog is simulated)
 
-  # Virtual block devices for A/B slot testing
-  # In QEMU, we use files as virtual disks
+  # ── RAUC slot device paths (virtio block devices) ──────────────────────────
+  # When launched with the test runner (scripts/run-qemu-rauc-test.sh), QEMU
+  # attaches four extra virtio-blk disks for A/B slot testing:
+  #   vdb = boot A (vfat, 128 MB)
+  #   vdc = boot B (vfat, 128 MB)
+  #   vdd = rootfs A (1 GB)
+  #   vde = rootfs B (1 GB)
+  #
+  # The primary disk (vda) is the NixOS root filesystem.
+  atomicnix.rauc.slots = {
+    boot0 = "/dev/vdb";
+    boot1 = "/dev/vdc";
+    rootfs0 = "/dev/vdd";
+    rootfs1 = "/dev/vde";
+  };
 }

modules/hardware-rock64.nix
@@ -104,4 +104,14 @@
     "mmc_block"
     "dw_mmc_rockchip"
   ];
+
+  # ── RAUC slot device paths (eMMC) ──────────────────────────────────────────
+  # These map to the GPT partition layout created by the provisioning script:
+  #   p1 = boot A (vfat), p2 = boot B (vfat), p3 = rootfs A, p4 = rootfs B
+  atomicnix.rauc.slots = {
+    boot0 = "/dev/mmcblk1p1";
+    boot1 = "/dev/mmcblk1p2";
+    rootfs0 = "/dev/mmcblk1p3";
+    rootfs1 = "/dev/mmcblk1p4";
+  };
 }

modules/rauc.nix
@@ -1,5 +1,7 @@
 # RAUC A/B update system configuration.
 # Defines slot pairs and U-Boot bootloader backend.
+# Slot device paths are configurable via options — each hardware module
+# (Rock64, QEMU) provides its own device paths.
 {
   config,
   lib,
@@ -7,53 +9,91 @@
   ...
 }:
 
+let
+  cfg = config.atomicnix.rauc;
+in
 {
-  # ── RAUC service ─────────────────────────────────────────────────────────────
+  # ── Options ─────────────────────────────────────────────────────────────────
 
-  # Note: The RAUC NixOS module handles most of this, but we also need
-  # to provide the system.conf for the slot definitions.
+  options.atomicnix.rauc = {
+    compatible = lib.mkOption {
+      type = lib.types.str;
+      default = "rock64";
+      description = "RAUC compatible string — must match the bundle manifest.";
+    };
 
-  environment.systemPackages = [ pkgs.rauc ];
+    bootloader = lib.mkOption {
+      type = lib.types.str;
+      default = "uboot";
+      description = "RAUC bootloader backend (uboot, custom, etc).";
+    };
 
-  # RAUC system configuration
-  # This defines the slot layout and bootloader integration
-  environment.etc."rauc/system.conf".text = ''
-    [system]
-    compatible=rock64
-    bootloader=uboot
-    statusfile=/persist/rauc/status.raucs
+    statusFile = lib.mkOption {
+      type = lib.types.str;
+      default = "/persist/rauc/status.raucs";
+      description = "Path to the RAUC status file.";
+    };
 
-    [keyring]
-    path=/etc/rauc/ca.cert.pem
+    slots = {
+      boot0 = lib.mkOption {
+        type = lib.types.str;
+        description = "Block device for boot slot A.";
+      };
+      boot1 = lib.mkOption {
+        type = lib.types.str;
+        description = "Block device for boot slot B.";
+      };
+      rootfs0 = lib.mkOption {
+        type = lib.types.str;
+        description = "Block device for rootfs slot A.";
+      };
+      rootfs1 = lib.mkOption {
+        type = lib.types.str;
+        description = "Block device for rootfs slot B.";
+      };
+    };
+  };
 
-    [handlers]
-    bootloader-custom-backend=noop
+  # ── Configuration ───────────────────────────────────────────────────────────
 
-    [slot.boot.0]
-    device=/dev/mmcblk1p1
-    type=vfat
-    bootname=A
+  config = {
+    environment.systemPackages = [ pkgs.rauc ];
 
-    [slot.rootfs.0]
-    device=/dev/mmcblk1p3
-    type=raw
-    bootname=A
-    parent=boot.0
+    # RAUC system configuration
+    # This defines the slot layout and bootloader integration
+    environment.etc."rauc/system.conf".text = ''
+      [system]
+      compatible=${cfg.compatible}
+      bootloader=${cfg.bootloader}
+      statusfile=${cfg.statusFile}
 
-    [slot.boot.1]
-    device=/dev/mmcblk1p2
-    type=vfat
-    bootname=B
+      [keyring]
+      path=/etc/rauc/ca.cert.pem
 
-    [slot.rootfs.1]
-    device=/dev/mmcblk1p4
-    type=raw
-    bootname=B
-    parent=boot.1
-  '';
+      [slot.boot.0]
+      device=${cfg.slots.boot0}
+      type=vfat
+      bootname=A
 
-  # CA certificate for bundle verification
-  # Uses the development CA by default. Production devices override this
-  # with a production CA cert provisioned separately.
-  environment.etc."rauc/ca.cert.pem".source = ../certs/dev.ca.cert.pem;
+      [slot.rootfs.0]
+      device=${cfg.slots.rootfs0}
+      type=raw
+      parent=boot.0
+
+      [slot.boot.1]
+      device=${cfg.slots.boot1}
+      type=vfat
+      bootname=B
+
+      [slot.rootfs.1]
+      device=${cfg.slots.rootfs1}
+      type=raw
+      parent=boot.1
+    '';
+
+    # CA certificate for bundle verification
+    # Uses the development CA by default. Production devices override this
+    # with a production CA cert provisioned separately.
+    environment.etc."rauc/ca.cert.pem".source = ../certs/dev.ca.cert.pem;
+  };
 }

nix/tests/rauc-slots.nix
@@ -1,0 +1,128 @@
+# NixOS test: verify RAUC slot logic with virtual block devices in QEMU.
+#
+# This test boots a minimal QEMU VM with four extra virtio-blk disks
+# representing the A/B slot pairs (boot-a, boot-b, rootfs-a, rootfs-b),
+# then verifies that RAUC sees all four slots with the correct device paths.
+#
+# Only imports rauc.nix + hardware-qemu.nix — no podman/cockpit/traefik.
+#
+# Run:  nix build .#checks.aarch64-linux.rauc-slots
+{
+  pkgs,
+  self,
+  raucModule,
+  qemuModule,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
+nixos-lib.runTest {
+  name = "rauc-slot-logic";
+
+  hostPkgs = pkgs;
+
+  nodes.gateway =
+    { config, lib, ... }:
+    {
+      imports = [
+        raucModule
+        qemuModule
+      ];
+
+      virtualisation = {
+        # Four extra virtio-blk disks for A/B slot pairs.
+        # NixOS test infra creates these as qcow2 images automatically.
+        emptyDiskImages = [
+          128 # vdb — boot slot A (128 MB)
+          128 # vdc — boot slot B (128 MB)
+          1024 # vdd — rootfs slot A (1 GB)
+          1024 # vde — rootfs slot B (1 GB)
+        ];
+        memorySize = 1024;
+        diskSize = 2048;
+      };
+
+      # Minimal system — just enough for RAUC to work
+      system.stateVersion = "25.11";
+      environment.systemPackages = [ pkgs.rauc ];
+
+      # Tell RAUC which slot is currently booted via kernel cmdline.
+      # Without this, `rauc service` cannot determine slot states.
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+
+      # Status file directory — the default /persist doesn't exist in
+      # the test VM, so override to a path that does.
+      atomicnix.rauc.statusFile = "/tmp/rauc.status";
+
+      # RAUC D-Bus service — needed so `rauc status` can query slot info.
+      # The upstream NixOS module (services.rauc) pulls in more than we need,
+      # so we define a minimal systemd unit directly.
+      systemd.services.rauc = {
+        description = "RAUC slot management service";
+        after = [ "dbus.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "dbus";
+          BusName = "de.pengutronix.rauc";
+          ExecStart = "${pkgs.rauc}/bin/rauc service --conf=/etc/rauc/system.conf";
+        };
+      };
+
+      # D-Bus policy so RAUC can register on the system bus
+      services.dbus.packages = [ pkgs.rauc ];
+    };
+
+  testScript = ''
+    gateway.start()
+    gateway.wait_for_unit("multi-user.target")
+
+    # Verify the four virtio block devices exist
+    gateway.succeed("test -b /dev/vdb")  # boot slot A
+    gateway.succeed("test -b /dev/vdc")  # boot slot B
+    gateway.succeed("test -b /dev/vdd")  # rootfs slot A
+    gateway.succeed("test -b /dev/vde")  # rootfs slot B
+
+    # Verify RAUC system.conf is deployed
+    gateway.succeed("test -f /etc/rauc/system.conf")
+
+    # Check system.conf references our virtio devices
+    conf = gateway.succeed("cat /etc/rauc/system.conf")
+    assert "/dev/vdb" in conf, f"boot slot A device not in system.conf: {conf}"
+    assert "/dev/vdc" in conf, f"boot slot B device not in system.conf: {conf}"
+    assert "/dev/vdd" in conf, f"rootfs slot A device not in system.conf: {conf}"
+    assert "/dev/vde" in conf, f"rootfs slot B device not in system.conf: {conf}"
+    assert "compatible=rock64" in conf, f"compatible string missing: {conf}"
+    assert "bootloader=uboot" in conf, f"bootloader setting missing: {conf}"
+
+    # Verify bootname labels and parent relationships
+    assert "bootname=A" in conf, f"bootname A missing: {conf}"
+    assert "bootname=B" in conf, f"bootname B missing: {conf}"
+    assert "parent=boot.0" in conf, f"rootfs.0 parent missing: {conf}"
+    assert "parent=boot.1" in conf, f"rootfs.1 parent missing: {conf}"
+
+    # Verify RAUC CA certificate is deployed
+    gateway.succeed("test -f /etc/rauc/ca.cert.pem")
+
+    # Wait for the RAUC D-Bus service to be ready
+    gateway.wait_for_unit("rauc.service")
+
+    # Verify RAUC can parse system.conf and list slot information.
+    # Disks are empty (no filesystems) so slots show as inactive — that's
+    # expected. The point is RAUC recognizes the slot structure.
+    status = gateway.succeed("rauc status 2>&1")
+
+    assert "boot.0" in status, f"boot.0 slot not in rauc status: {status}"
+    assert "boot.1" in status, f"boot.1 slot not in rauc status: {status}"
+    assert "rootfs.0" in status, f"rootfs.0 slot not in rauc status: {status}"
+    assert "rootfs.1" in status, f"rootfs.1 slot not in rauc status: {status}"
+
+    assert "/dev/vdb" in status, f"vdb not in rauc status: {status}"
+    assert "/dev/vdc" in status, f"vdc not in rauc status: {status}"
+    assert "/dev/vdd" in status, f"vdd not in rauc status: {status}"
+    assert "/dev/vde" in status, f"vde not in rauc status: {status}"
+
+    gateway.log("RAUC slot logic verification passed — all 4 slots detected with correct device paths")
+  '';
+}

RobertDeRose/atomixos

modules/hardware-qemu.nix
@@ -33,6 +33,19 @@
   # (systemd watchdog config from watchdog.nix still applies,
   #  but hardware watchdog is simulated)
 
-  # Virtual block devices for A/B slot testing
-  # In QEMU, we use files as virtual disks
+  # ── RAUC slot device paths (virtio block devices) ──────────────────────────
+  # When launched with the test runner (scripts/run-qemu-rauc-test.sh), QEMU
+  # attaches four extra virtio-blk disks for A/B slot testing:
+  #   vdb = boot A (vfat, 128 MB)
+  #   vdc = boot B (vfat, 128 MB)
+  #   vdd = rootfs A (1 GB)
+  #   vde = rootfs B (1 GB)
+  #
+  # The primary disk (vda) is the NixOS root filesystem.
+  atomixos.rauc.slots = {
+    boot0 = "/dev/vdb";
+    boot1 = "/dev/vdc";
+    rootfs0 = "/dev/vdd";
+    rootfs1 = "/dev/vde";
+  };
 }

modules/hardware-rock64.nix
@@ -104,4 +104,14 @@
     "mmc_block"
     "dw_mmc_rockchip"
   ];
+
+  # ── RAUC slot device paths (eMMC) ──────────────────────────────────────────
+  # These map to the GPT partition layout created by the provisioning script:
+  #   p1 = boot A (vfat), p2 = boot B (vfat), p3 = rootfs A, p4 = rootfs B
+  atomixos.rauc.slots = {
+    boot0 = "/dev/mmcblk1p1";
+    boot1 = "/dev/mmcblk1p2";
+    rootfs0 = "/dev/mmcblk1p3";
+    rootfs1 = "/dev/mmcblk1p4";
+  };
 }

modules/rauc.nix
@@ -1,5 +1,7 @@
 # RAUC A/B update system configuration.
 # Defines slot pairs and U-Boot bootloader backend.
+# Slot device paths are configurable via options — each hardware module
+# (Rock64, QEMU) provides its own device paths.
 {
   config,
   lib,
@@ -7,53 +9,91 @@
   ...
 }:
 
+let
+  cfg = config.atomixos.rauc;
+in
 {
-  # ── RAUC service ─────────────────────────────────────────────────────────────
+  # ── Options ─────────────────────────────────────────────────────────────────
 
-  # Note: The RAUC NixOS module handles most of this, but we also need
-  # to provide the system.conf for the slot definitions.
+  options.atomixos.rauc = {
+    compatible = lib.mkOption {
+      type = lib.types.str;
+      default = "rock64";
+      description = "RAUC compatible string — must match the bundle manifest.";
+    };
 
-  environment.systemPackages = [ pkgs.rauc ];
+    bootloader = lib.mkOption {
+      type = lib.types.str;
+      default = "uboot";
+      description = "RAUC bootloader backend (uboot, custom, etc).";
+    };
 
-  # RAUC system configuration
-  # This defines the slot layout and bootloader integration
-  environment.etc."rauc/system.conf".text = ''
-    [system]
-    compatible=rock64
-    bootloader=uboot
-    statusfile=/persist/rauc/status.raucs
+    statusFile = lib.mkOption {
+      type = lib.types.str;
+      default = "/persist/rauc/status.raucs";
+      description = "Path to the RAUC status file.";
+    };
 
-    [keyring]
-    path=/etc/rauc/ca.cert.pem
+    slots = {
+      boot0 = lib.mkOption {
+        type = lib.types.str;
+        description = "Block device for boot slot A.";
+      };
+      boot1 = lib.mkOption {
+        type = lib.types.str;
+        description = "Block device for boot slot B.";
+      };
+      rootfs0 = lib.mkOption {
+        type = lib.types.str;
+        description = "Block device for rootfs slot A.";
+      };
+      rootfs1 = lib.mkOption {
+        type = lib.types.str;
+        description = "Block device for rootfs slot B.";
+      };
+    };
+  };
 
-    [handlers]
-    bootloader-custom-backend=noop
+  # ── Configuration ───────────────────────────────────────────────────────────
 
-    [slot.boot.0]
-    device=/dev/mmcblk1p1
-    type=vfat
-    bootname=A
+  config = {
+    environment.systemPackages = [ pkgs.rauc ];
 
-    [slot.rootfs.0]
-    device=/dev/mmcblk1p3
-    type=raw
-    bootname=A
-    parent=boot.0
+    # RAUC system configuration
+    # This defines the slot layout and bootloader integration
+    environment.etc."rauc/system.conf".text = ''
+      [system]
+      compatible=${cfg.compatible}
+      bootloader=${cfg.bootloader}
+      statusfile=${cfg.statusFile}
 
-    [slot.boot.1]
-    device=/dev/mmcblk1p2
-    type=vfat
-    bootname=B
+      [keyring]
+      path=/etc/rauc/ca.cert.pem
 
-    [slot.rootfs.1]
-    device=/dev/mmcblk1p4
-    type=raw
-    bootname=B
-    parent=boot.1
-  '';
+      [slot.boot.0]
+      device=${cfg.slots.boot0}
+      type=vfat
+      bootname=A
 
-  # CA certificate for bundle verification
-  # Uses the development CA by default. Production devices override this
-  # with a production CA cert provisioned separately.
-  environment.etc."rauc/ca.cert.pem".source = ../certs/dev.ca.cert.pem;
+      [slot.rootfs.0]
+      device=${cfg.slots.rootfs0}
+      type=raw
+      parent=boot.0
+
+      [slot.boot.1]
+      device=${cfg.slots.boot1}
+      type=vfat
+      bootname=B
+
+      [slot.rootfs.1]
+      device=${cfg.slots.rootfs1}
+      type=raw
+      parent=boot.1
+    '';
+
+    # CA certificate for bundle verification
+    # Uses the development CA by default. Production devices override this
+    # with a production CA cert provisioned separately.
+    environment.etc."rauc/ca.cert.pem".source = ../certs/dev.ca.cert.pem;
+  };
 }

nix/tests/rauc-slots.nix
@@ -1,0 +1,128 @@
+# NixOS test: verify RAUC slot logic with virtual block devices in QEMU.
+#
+# This test boots a minimal QEMU VM with four extra virtio-blk disks
+# representing the A/B slot pairs (boot-a, boot-b, rootfs-a, rootfs-b),
+# then verifies that RAUC sees all four slots with the correct device paths.
+#
+# Only imports rauc.nix + hardware-qemu.nix — no podman/cockpit/traefik.
+#
+# Run:  nix build .#checks.aarch64-linux.rauc-slots
+{
+  pkgs,
+  self,
+  raucModule,
+  qemuModule,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
+nixos-lib.runTest {
+  name = "rauc-slot-logic";
+
+  hostPkgs = pkgs;
+
+  nodes.gateway =
+    { config, lib, ... }:
+    {
+      imports = [
+        raucModule
+        qemuModule
+      ];
+
+      virtualisation = {
+        # Four extra virtio-blk disks for A/B slot pairs.
+        # NixOS test infra creates these as qcow2 images automatically.
+        emptyDiskImages = [
+          128 # vdb — boot slot A (128 MB)
+          128 # vdc — boot slot B (128 MB)
+          1024 # vdd — rootfs slot A (1 GB)
+          1024 # vde — rootfs slot B (1 GB)
+        ];
+        memorySize = 1024;
+        diskSize = 2048;
+      };
+
+      # Minimal system — just enough for RAUC to work
+      system.stateVersion = "25.11";
+      environment.systemPackages = [ pkgs.rauc ];
+
+      # Tell RAUC which slot is currently booted via kernel cmdline.
+      # Without this, `rauc service` cannot determine slot states.
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+
+      # Status file directory — the default /persist doesn't exist in
+      # the test VM, so override to a path that does.
+      atomixos.rauc.statusFile = "/tmp/rauc.status";
+
+      # RAUC D-Bus service — needed so `rauc status` can query slot info.
+      # The upstream NixOS module (services.rauc) pulls in more than we need,
+      # so we define a minimal systemd unit directly.
+      systemd.services.rauc = {
+        description = "RAUC slot management service";
+        after = [ "dbus.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "dbus";
+          BusName = "de.pengutronix.rauc";
+          ExecStart = "${pkgs.rauc}/bin/rauc service --conf=/etc/rauc/system.conf";
+        };
+      };
+
+      # D-Bus policy so RAUC can register on the system bus
+      services.dbus.packages = [ pkgs.rauc ];
+    };
+
+  testScript = ''
+    gateway.start()
+    gateway.wait_for_unit("multi-user.target")
+
+    # Verify the four virtio block devices exist
+    gateway.succeed("test -b /dev/vdb")  # boot slot A
+    gateway.succeed("test -b /dev/vdc")  # boot slot B
+    gateway.succeed("test -b /dev/vdd")  # rootfs slot A
+    gateway.succeed("test -b /dev/vde")  # rootfs slot B
+
+    # Verify RAUC system.conf is deployed
+    gateway.succeed("test -f /etc/rauc/system.conf")
+
+    # Check system.conf references our virtio devices
+    conf = gateway.succeed("cat /etc/rauc/system.conf")
+    assert "/dev/vdb" in conf, f"boot slot A device not in system.conf: {conf}"
+    assert "/dev/vdc" in conf, f"boot slot B device not in system.conf: {conf}"
+    assert "/dev/vdd" in conf, f"rootfs slot A device not in system.conf: {conf}"
+    assert "/dev/vde" in conf, f"rootfs slot B device not in system.conf: {conf}"
+    assert "compatible=rock64" in conf, f"compatible string missing: {conf}"
+    assert "bootloader=uboot" in conf, f"bootloader setting missing: {conf}"
+
+    # Verify bootname labels and parent relationships
+    assert "bootname=A" in conf, f"bootname A missing: {conf}"
+    assert "bootname=B" in conf, f"bootname B missing: {conf}"
+    assert "parent=boot.0" in conf, f"rootfs.0 parent missing: {conf}"
+    assert "parent=boot.1" in conf, f"rootfs.1 parent missing: {conf}"
+
+    # Verify RAUC CA certificate is deployed
+    gateway.succeed("test -f /etc/rauc/ca.cert.pem")
+
+    # Wait for the RAUC D-Bus service to be ready
+    gateway.wait_for_unit("rauc.service")
+
+    # Verify RAUC can parse system.conf and list slot information.
+    # Disks are empty (no filesystems) so slots show as inactive — that's
+    # expected. The point is RAUC recognizes the slot structure.
+    status = gateway.succeed("rauc status 2>&1")
+
+    assert "boot.0" in status, f"boot.0 slot not in rauc status: {status}"
+    assert "boot.1" in status, f"boot.1 slot not in rauc status: {status}"
+    assert "rootfs.0" in status, f"rootfs.0 slot not in rauc status: {status}"
+    assert "rootfs.1" in status, f"rootfs.1 slot not in rauc status: {status}"
+
+    assert "/dev/vdb" in status, f"vdb not in rauc status: {status}"
+    assert "/dev/vdc" in status, f"vdc not in rauc status: {status}"
+    assert "/dev/vdd" in status, f"vdd not in rauc status: {status}"
+    assert "/dev/vde" in status, f"vde not in rauc status: {status}"
+
+    gateway.log("RAUC slot logic verification passed — all 4 slots detected with correct device paths")
+  '';
+}