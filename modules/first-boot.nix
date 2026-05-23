RobertDeRose/AtomicNIx

fix(modules): stabilize Rock64 boot and Overwatch transition
Stabilize first-boot storage provisioning and align the Rock64
image, boot flow, and docs around the current A/B layout.
Re-enable USB NIC kernel support and update related docs so the
LAN/WAN hardware assumptions match the image again.

Remove the local Cockpit/Traefik management stack and pivot the
device model toward Overwatch-managed remote access. Keep Podman
available for application workloads and simplify on-device
verification to local gateway health checks.

modules/first-boot.nix
@@ -2,7 +2,7 @@
 #
 # Marks the RAUC slot as good unconditionally (no health manifest or
 # container images exist yet on first boot) and writes a sentinel file
-# (/persist/.completed_first_boot) so it never runs again.
+# (/data/.completed_first_boot) so it never runs again.
 #
 # os-verification.service has the inverse condition — it only runs after
 # the sentinel exists (i.e. on all boots AFTER the first).
@@ -11,29 +11,54 @@
   lib,
   pkgs,
   self,
+  developmentMode ? false,
+  developmentAdminPasswordHashFile ? "",
   ...
 }:
 
 let
   firstBootScript = pkgs.writeShellScript "first-boot" (builtins.readFile ../scripts/first-boot.sh);
-  ubootEnvTools = self.packages.${pkgs.system}.uboot-env-tools;
+  ubootEnvTools = self.packages.${pkgs.stdenv.hostPlatform.system}.uboot-env-tools;
+  trimmedDevAdminPasswordHash = lib.strings.trim devAdminPasswordHash;
+  devAdminPasswordHash =
+    if
+      developmentMode
+      && developmentAdminPasswordHashFile != ""
+      && builtins.pathExists developmentAdminPasswordHashFile
+    then
+      builtins.readFile developmentAdminPasswordHashFile
+    else
+      "";
+  firstBootEnv =
+    lib.optionalAttrs (trimmedDevAdminPasswordHash != "") {
+      ATOMICNIX_DEV_ADMIN_PASSWORD_HASH = trimmedDevAdminPasswordHash;
+    }
+    // lib.optionalAttrs developmentMode {
+      ATOMICNIX_DEV_ENABLE_SSH_WAN = "1";
+    };
 in
 {
   systemd.services.first-boot = {
     description = "First-boot initialization (mark slot good, write sentinel)";
     after = [
+      "data.mount"
       "multi-user.target"
     ];
+    wants = [ "data.mount" ];
     wantedBy = [ "multi-user.target" ];
 
     # Only run if the sentinel does NOT exist (first boot only)
-    unitConfig.ConditionPathExists = "!/persist/.completed_first_boot";
+    unitConfig.ConditionPathExists = "!/data/.completed_first_boot";
+    unitConfig.RequiresMountsFor = [ "/data" ];
 
     # RAUC needs to be on PATH to call `rauc status mark-good`
     path = [
       pkgs.rauc
+      pkgs.coreutils
+      pkgs.systemd
       ubootEnvTools
     ];
+    environment = firstBootEnv;
 
     serviceConfig = {
       Type = "oneshot";

scripts/boot.cmd
@@ -4,8 +4,8 @@
 # decrement, and saveenv BEFORE loading this script. By the time this runs:
 #
 #   devnum           - MMC device number (1 for eMMC)
-#   distro_bootpart  - Boot partition of selected slot (1=A, 2=B)
-#   distro_rootpart  - Root partition of selected slot (3=A, 4=B)
+#   distro_bootpart  - Boot partition of selected slot (1=A, 3=B)
+#   distro_rootpart  - Root partition of selected slot (2=A, 4=B)
 #
 # This script only needs to load kernel/DTB/initrd and set bootargs.
 #
@@ -83,14 +83,14 @@ fi
 
 # Fallback: if RAUC bootmeth didn't set distro_rootpart (e.g. env_save failed
 # on first boot with uninitialized SPI flash), derive it from distro_bootpart.
-# Boot partition 1 → rootfs partition 3, boot partition 2 → rootfs partition 4.
+# Boot partition 1 → rootfs partition 2, boot partition 3 → rootfs partition 4.
 if test -z "${distro_rootpart}"; then
   if test "${distro_bootpart}" = "1"; then
-    setenv distro_rootpart 3
-  elif test "${distro_bootpart}" = "2"; then
+    setenv distro_rootpart 2
+  elif test "${distro_bootpart}" = "3"; then
     setenv distro_rootpart 4
   else
-    setenv distro_rootpart 3
+    setenv distro_rootpart 2
   fi
   echo "WARNING: distro_rootpart was empty, defaulted to ${distro_rootpart}"
 fi
@@ -99,7 +99,7 @@ fi
 # rootfs slot, so mark-good can target BOOT_A_LEFT / BOOT_B_LEFT correctly.
 if test "${distro_bootpart}" = "1"; then
   setenv rauc_slot boot.0
-elif test "${distro_bootpart}" = "2"; then
+elif test "${distro_bootpart}" = "3"; then
   setenv rauc_slot boot.1
 else
   setenv rauc_slot boot.0
@@ -120,7 +120,7 @@ fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} dtbs/rockchip/rk3328-rock
 fatload mmc ${devnum}:${distro_bootpart} ${ramdisk_addr_r} initrd
 setenv initrd_size ${filesize}
 
-setenv bootargs "${bootargs_base} root=/dev/mmcblk1p${distro_rootpart} rootfstype=squashfs ro rauc.slot=${rauc_slot}"
+setenv bootargs "${bootargs_base} root=fstab rw atomicnix.lowerdev=/dev/mmcblk1p${distro_rootpart} atomicnix.lowerfstype=squashfs init=@systemClosure@/init rauc.slot=${rauc_slot}"
 booti ${kernel_addr_r} ${ramdisk_addr_r}:${initrd_size} ${fdt_addr_r}
 
 echo "ERROR: booti failed!"

scripts/first-boot.sh
@@ -9,6 +9,32 @@ set -euo pipefail
 
 log() { echo "[first-boot] $*"; }
 
+write_dev_admin_password_hash() {
+	local hash_file="/data/config/admin-password-hash"
+	local hash_value="${ATOMICNIX_DEV_ADMIN_PASSWORD_HASH:-}"
+
+	if [ -z "$hash_value" ] || [ -f "$hash_file" ]; then
+		return 0
+	fi
+
+	mkdir -p "$(dirname "$hash_file")"
+	log "Writing development admin password hash"
+	printf '%s\n' "$hash_value" >"$hash_file"
+	chmod 600 "$hash_file"
+}
+
+enable_dev_ssh_on_wan() {
+	local flag_file="/data/config/ssh-wan-enabled"
+
+	if [ "${ATOMICNIX_DEV_ENABLE_SSH_WAN:-}" != "1" ] || [ -f "$flag_file" ]; then
+		return 0
+	fi
+
+	mkdir -p "$(dirname "$flag_file")"
+	log "Enabling SSH on WAN for development image"
+	: >"$flag_file"
+}
+
 ensure_rauc_env() {
 	if fw_printenv BOOT_ORDER >/dev/null 2>&1; then
 		return 0
@@ -33,7 +59,7 @@ current_boot_slot() {
 	return 1
 }
 
-SENTINEL="/persist/.completed_first_boot"
+SENTINEL="/data/.completed_first_boot"
 
 # Guard (belt-and-suspenders alongside systemd ConditionPathExists)
 if [ -f "$SENTINEL" ]; then
@@ -52,6 +78,9 @@ ensure_rauc_env
 
 log "Marking current slot as good via RAUC: $BOOT_SLOT"
 rauc status mark-good "$BOOT_SLOT"
+
+write_dev_admin_password_hash
+enable_dev_ssh_on_wan
 
 # ── Write sentinel ──
 log "Writing first-boot sentinel: $SENTINEL"

RobertDeRose/atomixos

modules/first-boot.nix
@@ -2,7 +2,7 @@
 #
 # Marks the RAUC slot as good unconditionally (no health manifest or
 # container images exist yet on first boot) and writes a sentinel file
-# (/persist/.completed_first_boot) so it never runs again.
+# (/data/.completed_first_boot) so it never runs again.
 #
 # os-verification.service has the inverse condition — it only runs after
 # the sentinel exists (i.e. on all boots AFTER the first).
@@ -11,29 +11,54 @@
   lib,
   pkgs,
   self,
+  developmentMode ? false,
+  developmentAdminPasswordHashFile ? "",
   ...
 }:
 
 let
   firstBootScript = pkgs.writeShellScript "first-boot" (builtins.readFile ../scripts/first-boot.sh);
-  ubootEnvTools = self.packages.${pkgs.system}.uboot-env-tools;
+  ubootEnvTools = self.packages.${pkgs.stdenv.hostPlatform.system}.uboot-env-tools;
+  trimmedDevAdminPasswordHash = lib.strings.trim devAdminPasswordHash;
+  devAdminPasswordHash =
+    if
+      developmentMode
+      && developmentAdminPasswordHashFile != ""
+      && builtins.pathExists developmentAdminPasswordHashFile
+    then
+      builtins.readFile developmentAdminPasswordHashFile
+    else
+      "";
+  firstBootEnv =
+    lib.optionalAttrs (trimmedDevAdminPasswordHash != "") {
+      ATOMIXOS_DEV_ADMIN_PASSWORD_HASH = trimmedDevAdminPasswordHash;
+    }
+    // lib.optionalAttrs developmentMode {
+      ATOMIXOS_DEV_ENABLE_SSH_WAN = "1";
+    };
 in
 {
   systemd.services.first-boot = {
     description = "First-boot initialization (mark slot good, write sentinel)";
     after = [
+      "data.mount"
       "multi-user.target"
     ];
+    wants = [ "data.mount" ];
     wantedBy = [ "multi-user.target" ];
 
     # Only run if the sentinel does NOT exist (first boot only)
-    unitConfig.ConditionPathExists = "!/persist/.completed_first_boot";
+    unitConfig.ConditionPathExists = "!/data/.completed_first_boot";
+    unitConfig.RequiresMountsFor = [ "/data" ];
 
     # RAUC needs to be on PATH to call `rauc status mark-good`
     path = [
       pkgs.rauc
+      pkgs.coreutils
+      pkgs.systemd
       ubootEnvTools
     ];
+    environment = firstBootEnv;
 
     serviceConfig = {
       Type = "oneshot";

scripts/boot.cmd
@@ -4,8 +4,8 @@
 # decrement, and saveenv BEFORE loading this script. By the time this runs:
 #
 #   devnum           - MMC device number (1 for eMMC)
-#   distro_bootpart  - Boot partition of selected slot (1=A, 2=B)
-#   distro_rootpart  - Root partition of selected slot (3=A, 4=B)
+#   distro_bootpart  - Boot partition of selected slot (1=A, 3=B)
+#   distro_rootpart  - Root partition of selected slot (2=A, 4=B)
 #
 # This script only needs to load kernel/DTB/initrd and set bootargs.
 #
@@ -83,14 +83,14 @@ fi
 
 # Fallback: if RAUC bootmeth didn't set distro_rootpart (e.g. env_save failed
 # on first boot with uninitialized SPI flash), derive it from distro_bootpart.
-# Boot partition 1 → rootfs partition 3, boot partition 2 → rootfs partition 4.
+# Boot partition 1 → rootfs partition 2, boot partition 3 → rootfs partition 4.
 if test -z "${distro_rootpart}"; then
   if test "${distro_bootpart}" = "1"; then
-    setenv distro_rootpart 3
-  elif test "${distro_bootpart}" = "2"; then
+    setenv distro_rootpart 2
+  elif test "${distro_bootpart}" = "3"; then
     setenv distro_rootpart 4
   else
-    setenv distro_rootpart 3
+    setenv distro_rootpart 2
   fi
   echo "WARNING: distro_rootpart was empty, defaulted to ${distro_rootpart}"
 fi
@@ -99,7 +99,7 @@ fi
 # rootfs slot, so mark-good can target BOOT_A_LEFT / BOOT_B_LEFT correctly.
 if test "${distro_bootpart}" = "1"; then
   setenv rauc_slot boot.0
-elif test "${distro_bootpart}" = "2"; then
+elif test "${distro_bootpart}" = "3"; then
   setenv rauc_slot boot.1
 else
   setenv rauc_slot boot.0
@@ -120,7 +120,7 @@ fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} dtbs/rockchip/rk3328-rock
 fatload mmc ${devnum}:${distro_bootpart} ${ramdisk_addr_r} initrd
 setenv initrd_size ${filesize}
 
-setenv bootargs "${bootargs_base} root=/dev/mmcblk1p${distro_rootpart} rootfstype=squashfs ro rauc.slot=${rauc_slot}"
+setenv bootargs "${bootargs_base} root=fstab rw atomixos.lowerdev=/dev/mmcblk1p${distro_rootpart} atomixos.lowerfstype=squashfs init=@systemClosure@/init rauc.slot=${rauc_slot}"
 booti ${kernel_addr_r} ${ramdisk_addr_r}:${initrd_size} ${fdt_addr_r}
 
 echo "ERROR: booti failed!"

scripts/first-boot.sh
@@ -9,6 +9,32 @@ set -euo pipefail
 
 log() { echo "[first-boot] $*"; }
 
+write_dev_admin_password_hash() {
+	local hash_file="/data/config/admin-password-hash"
+	local hash_value="${ATOMIXOS_DEV_ADMIN_PASSWORD_HASH:-}"
+
+	if [ -z "$hash_value" ] || [ -f "$hash_file" ]; then
+		return 0
+	fi
+
+	mkdir -p "$(dirname "$hash_file")"
+	log "Writing development admin password hash"
+	printf '%s\n' "$hash_value" >"$hash_file"
+	chmod 600 "$hash_file"
+}
+
+enable_dev_ssh_on_wan() {
+	local flag_file="/data/config/ssh-wan-enabled"
+
+	if [ "${ATOMIXOS_DEV_ENABLE_SSH_WAN:-}" != "1" ] || [ -f "$flag_file" ]; then
+		return 0
+	fi
+
+	mkdir -p "$(dirname "$flag_file")"
+	log "Enabling SSH on WAN for development image"
+	: >"$flag_file"
+}
+
 ensure_rauc_env() {
 	if fw_printenv BOOT_ORDER >/dev/null 2>&1; then
 		return 0
@@ -33,7 +59,7 @@ current_boot_slot() {
 	return 1
 }
 
-SENTINEL="/persist/.completed_first_boot"
+SENTINEL="/data/.completed_first_boot"
 
 # Guard (belt-and-suspenders alongside systemd ConditionPathExists)
 if [ -f "$SENTINEL" ]; then
@@ -52,6 +78,9 @@ ensure_rauc_env
 
 log "Marking current slot as good via RAUC: $BOOT_SLOT"
 rauc status mark-good "$BOOT_SLOT"
+
+write_dev_admin_password_hash
+enable_dev_ssh_on_wan
 
 # ── Write sentinel ──
 log "Writing first-boot sentinel: $SENTINEL"