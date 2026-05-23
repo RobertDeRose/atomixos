RobertDeRose/AtomicNIx

feat(modules): bounded forensic boot logging
Add slot-local /boot forensic logs for critical lifecycle events
with boot-scoped ordering, rotation, and durable writes.

Keep host and container journald logs volatile with a 64 MiB
runtime cap, and update tests and OpenSpec to match.

nix/tests/forensics-mount-selection.nix
@@ -1,0 +1,58 @@
+{
+  pkgs,
+  hostPkgs ? pkgs,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
+nixos-lib.runTest {
+  name = "forensics-mount-selection";
+
+  inherit hostPkgs;
+
+  nodes.machine =
+    {
+      ...
+    }:
+    {
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+      environment.systemPackages = [ pkgs.util-linux ];
+      environment.etc."atomicnix/current-boot-forensics-mount".source =
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          set -euo pipefail
+          slot="''${1:-}"
+          if [ -z "$slot" ]; then
+            slot="boot.0"
+          fi
+
+          case "$slot" in
+            boot.0)
+              if ${pkgs.util-linux}/bin/findmnt /run/forensics/boot.0 >/dev/null 2>&1; then
+                printf '%s\n' /run/forensics/boot.0
+              else
+                printf '%s\n' /boot
+              fi
+              ;;
+            *)
+              printf '%s\n' /boot
+              ;;
+          esac
+        '';
+      systemd.tmpfiles.rules = [
+        "d /run/forensics 0755 root root -"
+        "d /run/forensics/boot.0 0755 root root -"
+        "d /boot 0755 root root -"
+      ];
+    };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+
+    machine.succeed("test -d /run/forensics/boot.0")
+    machine.fail("findmnt /run/forensics/boot.0")
+    machine.succeed("test \"$(/etc/atomicnix/current-boot-forensics-mount boot.0)\" = /boot")
+  '';
+}

nix/tests/rauc-update.nix
@@ -6,6 +6,7 @@
 # 3. Builds a minimal RAUC bundle with dummy boot + rootfs images
 # 4. Installs the bundle via `rauc install`
 # 5. Verifies RAUC switched the primary to boot.1 (B)
+# 6. Verifies forensic records on the previously active slot remain readable
 #
 # Only imports rauc.nix + hardware-qemu.nix — no podman/cockpit/traefik.
 #
@@ -21,6 +22,7 @@
 
 let
   nixos-lib = import (pkgs.path + "/nixos/lib") { };
+  forensicScript = builtins.readFile ../../scripts/forensic-log.sh;
 
   signingCert = ../../certs/dev.signing.cert.pem;
   signingKey = ../../certs/dev.signing.key.pem;
@@ -100,7 +102,20 @@ nixos-lib.runTest {
       };
 
       system.stateVersion = "25.11";
-      environment.systemPackages = [ pkgs.rauc ];
+      environment.systemPackages = [
+        pkgs.rauc
+        pkgs.dosfstools
+        (pkgs.writeShellScriptBin "forensic-log" forensicScript)
+      ];
+      environment.etc."atomicnix/current-boot-forensics-mount".source =
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          set -euo pipefail
+          case "''${1:-}" in
+            boot.0) printf '%s\n' /run/forensics/boot.0 ;;
+            boot.1) printf '%s\n' /run/forensics/boot.1 ;;
+            *) printf '%s\n' /boot ;;
+          esac
+        '';
 
       boot.kernelParams = [ "rauc.slot=boot.0" ];
       atomicnix.rauc.statusFile = "/tmp/rauc.status";
@@ -115,6 +130,12 @@ nixos-lib.runTest {
     gateway.wait_for_unit("multi-user.target")
     gateway.wait_for_unit("rauc.service")
 
+    # The extra QEMU slot disks start blank, so prepare VFAT boot filesystems
+    # before checking the slot-local forensic mounts.
+    gateway.succeed("mkfs.vfat -n BOOTA /dev/vdb")
+    gateway.succeed("mkdir -p /run/forensics/boot.0 /run/forensics/boot.1")
+    gateway.succeed("mount -t vfat /dev/vdb /run/forensics/boot.0")
+
     # Verify we booted into slot A
     status = gateway.succeed("rauc status --output-format=json 2>&1")
     assert '"booted": "A"' in status or '"booted":"A"' in status or "booted" in status, \
@@ -123,6 +144,10 @@ nixos-lib.runTest {
     # Verify slot A is primary
     primary = gateway.succeed("cat /var/lib/rauc/primary 2>/dev/null || echo A").strip()
     assert primary == "A", f"Expected primary=A, got: {primary}"
+
+    # Record a Tier 0 event on the active slot before switching primary.
+    gateway.succeed("forensic-log --stage rauc --event install-start --slot boot.0 --detail pre-switch && sync")
+    gateway.succeed("grep 'slot=boot.0 stage=rauc event=install-start detail=pre-switch' /run/forensics/boot.0/forensics/segment-0.log")
 
     # Copy the test bundle into the VM
     gateway.succeed("mkdir -p /tmp/bundles")
@@ -135,9 +160,12 @@ nixos-lib.runTest {
     primary_after = gateway.succeed("cat /var/lib/rauc/primary").strip()
     assert primary_after == "B", f"Expected primary=B after install, got: {primary_after}"
 
+    # The previous slot's forensic ring must remain readable after the switch.
+    gateway.succeed("grep 'slot=boot.0 stage=rauc event=install-start detail=pre-switch' /run/forensics/boot.0/forensics/segment-0.log")
+
     # Verify rauc status shows the update
     status_after = gateway.succeed("rauc status --output-format=json 2>&1")
 
-    gateway.log("RAUC update install test passed — bundle installed to inactive slot, primary switched to B")
+    gateway.log("RAUC update install test passed — bundle installed to inactive slot, primary switched to B, and slot-local forensics remained readable")
   '';
 }

scripts/forensics-initrd-log.sh
@@ -1,0 +1,112 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+usage() {
+	cat <<'EOF'
+Usage:
+  forensics-initrd-log --event <event>
+EOF
+}
+
+EVENT=""
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+	--event)
+		EVENT="$2"
+		shift 2
+		;;
+	-h | --help)
+		usage
+		exit 0
+		;;
+	*)
+		printf 'Unknown argument: %s\n' "$1" >&2
+		usage >&2
+		exit 1
+		;;
+	esac
+done
+
+if [ -z "$EVENT" ]; then
+	usage >&2
+	exit 1
+fi
+
+slot="${ATOMICNIX_FORENSICS_SLOT:-}"
+lower_device="${ATOMICNIX_FORENSICS_LOWERDEV:-}"
+
+if [ -z "$slot" ] || [ -z "$lower_device" ]; then
+	for arg in $(</proc/cmdline); do
+		case "$arg" in
+		rauc.slot=boot.*)
+			if [ -z "$slot" ]; then
+				slot="${arg#rauc.slot=}"
+			fi
+			;;
+		atomicnix.lowerdev=*)
+			if [ -z "$lower_device" ]; then
+				lower_device="${arg#atomicnix.lowerdev=}"
+			fi
+			;;
+		esac
+	done
+fi
+
+if [ -z "$slot" ]; then
+	slot="boot.0"
+fi
+
+case "$slot" in
+boot.0)
+	boot_device="${ATOMICNIX_FORENSICS_BOOT0:-}"
+	;;
+boot.1)
+	boot_device="${ATOMICNIX_FORENSICS_BOOT1:-}"
+	;;
+*)
+	boot_device=""
+	;;
+esac
+
+mount_dir="${ATOMICNIX_FORENSICS_MOUNT:-/run/forensics-initrd}"
+mount_override=0
+if [ -n "${ATOMICNIX_FORENSICS_MOUNT:-}" ]; then
+	mount_override=1
+fi
+
+if [ "$mount_override" -eq 0 ] && [ -z "$boot_device" ]; then
+	printf 'forensics-initrd-log: missing boot device for slot %s\n' "$slot" >&2
+	exit 1
+fi
+
+mounted_here=0
+
+cleanup() {
+	if [ "$mounted_here" -eq 1 ]; then
+		umount "$mount_dir" >/dev/null 2>&1 || true
+	fi
+}
+
+trap cleanup EXIT INT TERM
+mkdir -p "$mount_dir"
+
+if [ "$mount_override" -eq 0 ] && ! findmnt "$mount_dir" >/dev/null 2>&1 && [ -n "$boot_device" ]; then
+	if ! mount -t vfat -o uid=0,gid=0,fmask=0133,dmask=0022 "$boot_device" "$mount_dir" >/dev/null 2>&1; then
+		printf 'forensics-initrd-log: failed to mount %s at %s\n' "$boot_device" "$mount_dir" >&2
+		exit 1
+	fi
+	mounted_here=1
+fi
+
+if [ "$EVENT" = "lowerdev-selected" ] && [ -z "$lower_device" ]; then
+	printf 'forensics-initrd-log: missing lower device for %s\n' "$EVENT" >&2
+	exit 1
+fi
+
+args=(--mount "$mount_dir" --slot "$slot" --stage initrd --event "$EVENT")
+if [ -n "$lower_device" ]; then
+	args+=(--device "$lower_device")
+fi
+
+forensic-log "${args[@]}"

scripts/os-verification.sh
@@ -5,10 +5,16 @@ set -euo pipefail
 
 # Dependencies (must be on PATH): rauc, jq, systemctl, ip
 
-SUSTAIN_DURATION=60 # 60 seconds
-CHECK_INTERVAL=5    # check every 5s during sustain
+SUSTAIN_DURATION="${ATOMICNIX_VERIFICATION_SUSTAIN_DURATION:-60}"
+CHECK_INTERVAL="${ATOMICNIX_VERIFICATION_CHECK_INTERVAL:-5}"
 
 log() { echo "[os-verification] $*"; }
+
+forensic() {
+	if command -v forensic-log >/dev/null 2>&1; then
+		forensic-log "$@" || true
+	fi
+}
 
 current_boot_slot() {
 	local arg
@@ -32,20 +38,42 @@ if [ -z "$SLOT_STATUS" ]; then
 	BOOT_SLOT="$(current_boot_slot || true)"
 	if [ -z "$BOOT_SLOT" ]; then
 		log "Could not determine boot slot from /proc/cmdline"
+		forensic --stage verify --event failed --reason missing-boot-slot
 		exit 1
 	fi
 	log "Assuming first boot, marking good: $BOOT_SLOT"
-	rauc status mark-good "$BOOT_SLOT" || true
-	exit 0
+	forensic --stage verify --event start --slot "$BOOT_SLOT"
+	forensic --stage rauc --event mark-good-start --slot "$BOOT_SLOT"
+	if rauc status mark-good "$BOOT_SLOT"; then
+		forensic --stage rauc --event mark-good-complete --slot "$BOOT_SLOT" --result ok
+		forensic --stage verify --event complete --slot "$BOOT_SLOT" --result ok
+		exit 0
+	fi
+	forensic --stage rauc --event mark-good-failed --slot "$BOOT_SLOT" --reason initial-boot
+	forensic --stage verify --event failed --slot "$BOOT_SLOT" --reason mark-good-failed
+	exit 1
 fi
 
-BOOT_GOOD=$(printf '%s\n' "$RAUC_STATUS_JSON" | jq -r '.slots[] | select(.state.booted == "booted") | .state.boot_status // "unknown"' 2>/dev/null || true)
+BOOT_GOOD=$(printf '%s\n' "$RAUC_STATUS_JSON" | jq -r '
+	.booted as $booted
+	| .slots[]
+	| to_entries[]
+	| select(.key == $booted)
+	| .value.boot_status // "unknown"
+' 2>/dev/null || true)
 if [ "$BOOT_GOOD" = "good" ]; then
 	log "Slot already marked good, nothing to do"
+	forensic --stage verify --event complete --result already-good
 	exit 0
 fi
 
 log "Slot is pending confirmation, running health checks..."
+BOOT_SLOT="$(current_boot_slot || true)"
+if [ -z "$BOOT_SLOT" ]; then
+	forensic --stage verify --event failed --reason missing-boot-slot
+	exit 1
+fi
+forensic --stage verify --event start --slot "$BOOT_SLOT"
 
 # ── Step 2: System health checks ──
 check_service() {
@@ -85,6 +113,7 @@ fi
 
 if [ "$SYSTEM_OK" != "true" ]; then
 	log "FAIL: System health checks failed"
+	forensic --stage verify --event failed --slot "$BOOT_SLOT" --reason system-health
 	exit 1
 fi
 
@@ -94,13 +123,14 @@ log "System health checks passed"
 log "Starting sustained health check (${SUSTAIN_DURATION}s)..."
 
 ELAPSED=0
-while [ $ELAPSED -lt $SUSTAIN_DURATION ]; do
-	sleep $CHECK_INTERVAL
+while [ "$ELAPSED" -lt "$SUSTAIN_DURATION" ]; do
+	sleep "$CHECK_INTERVAL"
 	ELAPSED=$((ELAPSED + CHECK_INTERVAL))
 
 	# Check system services still up
 	if ! systemctl is-active --quiet "dnsmasq.service" 2>/dev/null; then
 		log "FAIL: dnsmasq stopped during sustained check"
+		forensic --stage verify --event failed --slot "$BOOT_SLOT" --reason sustained-check
 		exit 1
 	fi
 done
@@ -111,9 +141,19 @@ log "Sustained health check passed (${SUSTAIN_DURATION}s)"
 BOOT_SLOT="$(current_boot_slot || true)"
 if [ -z "$BOOT_SLOT" ]; then
 	log "Could not determine boot slot from /proc/cmdline"
+	forensic --stage verify --event failed --reason missing-boot-slot
 	exit 1
 fi
 
 log "All checks passed, marking slot as good: $BOOT_SLOT"
-rauc status mark-good "$BOOT_SLOT"
-log "Slot committed successfully"
+forensic --stage rauc --event mark-good-start --slot "$BOOT_SLOT"
+if rauc status mark-good "$BOOT_SLOT"; then
+	forensic --stage rauc --event mark-good-complete --slot "$BOOT_SLOT" --result ok
+	forensic --stage verify --event complete --slot "$BOOT_SLOT" --result ok
+	log "Slot committed successfully"
+	exit 0
+fi
+
+forensic --stage rauc --event mark-good-failed --slot "$BOOT_SLOT" --reason health-check
+forensic --stage verify --event failed --slot "$BOOT_SLOT" --reason mark-good-failed
+exit 1

RobertDeRose/atomixos

nix/tests/forensics-mount-selection.nix
@@ -1,0 +1,58 @@
+{
+  pkgs,
+  hostPkgs ? pkgs,
+  ...
+}:
+
+let
+  nixos-lib = import (pkgs.path + "/nixos/lib") { };
+in
+nixos-lib.runTest {
+  name = "forensics-mount-selection";
+
+  inherit hostPkgs;
+
+  nodes.machine =
+    {
+      ...
+    }:
+    {
+      boot.kernelParams = [ "rauc.slot=boot.0" ];
+      environment.systemPackages = [ pkgs.util-linux ];
+      environment.etc."atomixos/current-boot-forensics-mount".source =
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          set -euo pipefail
+          slot="''${1:-}"
+          if [ -z "$slot" ]; then
+            slot="boot.0"
+          fi
+
+          case "$slot" in
+            boot.0)
+              if ${pkgs.util-linux}/bin/findmnt /run/forensics/boot.0 >/dev/null 2>&1; then
+                printf '%s\n' /run/forensics/boot.0
+              else
+                printf '%s\n' /boot
+              fi
+              ;;
+            *)
+              printf '%s\n' /boot
+              ;;
+          esac
+        '';
+      systemd.tmpfiles.rules = [
+        "d /run/forensics 0755 root root -"
+        "d /run/forensics/boot.0 0755 root root -"
+        "d /boot 0755 root root -"
+      ];
+    };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+
+    machine.succeed("test -d /run/forensics/boot.0")
+    machine.fail("findmnt /run/forensics/boot.0")
+    machine.succeed("test \"$(/etc/atomixos/current-boot-forensics-mount boot.0)\" = /boot")
+  '';
+}

nix/tests/rauc-update.nix
@@ -6,6 +6,7 @@
 # 3. Builds a minimal RAUC bundle with dummy boot + rootfs images
 # 4. Installs the bundle via `rauc install`
 # 5. Verifies RAUC switched the primary to boot.1 (B)
+# 6. Verifies forensic records on the previously active slot remain readable
 #
 # Only imports rauc.nix + hardware-qemu.nix — no podman/cockpit/traefik.
 #
@@ -21,6 +22,7 @@
 
 let
   nixos-lib = import (pkgs.path + "/nixos/lib") { };
+  forensicScript = builtins.readFile ../../scripts/forensic-log.sh;
 
   signingCert = ../../certs/dev.signing.cert.pem;
   signingKey = ../../certs/dev.signing.key.pem;
@@ -100,7 +102,20 @@ nixos-lib.runTest {
       };
 
       system.stateVersion = "25.11";
-      environment.systemPackages = [ pkgs.rauc ];
+      environment.systemPackages = [
+        pkgs.rauc
+        pkgs.dosfstools
+        (pkgs.writeShellScriptBin "forensic-log" forensicScript)
+      ];
+      environment.etc."atomixos/current-boot-forensics-mount".source =
+        pkgs.writeShellScript "current-boot-forensics-mount" ''
+          set -euo pipefail
+          case "''${1:-}" in
+            boot.0) printf '%s\n' /run/forensics/boot.0 ;;
+            boot.1) printf '%s\n' /run/forensics/boot.1 ;;
+            *) printf '%s\n' /boot ;;
+          esac
+        '';
 
       boot.kernelParams = [ "rauc.slot=boot.0" ];
       atomixos.rauc.statusFile = "/tmp/rauc.status";
@@ -115,6 +130,12 @@ nixos-lib.runTest {
     gateway.wait_for_unit("multi-user.target")
     gateway.wait_for_unit("rauc.service")
 
+    # The extra QEMU slot disks start blank, so prepare VFAT boot filesystems
+    # before checking the slot-local forensic mounts.
+    gateway.succeed("mkfs.vfat -n BOOTA /dev/vdb")
+    gateway.succeed("mkdir -p /run/forensics/boot.0 /run/forensics/boot.1")
+    gateway.succeed("mount -t vfat /dev/vdb /run/forensics/boot.0")
+
     # Verify we booted into slot A
     status = gateway.succeed("rauc status --output-format=json 2>&1")
     assert '"booted": "A"' in status or '"booted":"A"' in status or "booted" in status, \
@@ -123,6 +144,10 @@ nixos-lib.runTest {
     # Verify slot A is primary
     primary = gateway.succeed("cat /var/lib/rauc/primary 2>/dev/null || echo A").strip()
     assert primary == "A", f"Expected primary=A, got: {primary}"
+
+    # Record a Tier 0 event on the active slot before switching primary.
+    gateway.succeed("forensic-log --stage rauc --event install-start --slot boot.0 --detail pre-switch && sync")
+    gateway.succeed("grep 'slot=boot.0 stage=rauc event=install-start detail=pre-switch' /run/forensics/boot.0/forensics/segment-0.log")
 
     # Copy the test bundle into the VM
     gateway.succeed("mkdir -p /tmp/bundles")
@@ -135,9 +160,12 @@ nixos-lib.runTest {
     primary_after = gateway.succeed("cat /var/lib/rauc/primary").strip()
     assert primary_after == "B", f"Expected primary=B after install, got: {primary_after}"
 
+    # The previous slot's forensic ring must remain readable after the switch.
+    gateway.succeed("grep 'slot=boot.0 stage=rauc event=install-start detail=pre-switch' /run/forensics/boot.0/forensics/segment-0.log")
+
     # Verify rauc status shows the update
     status_after = gateway.succeed("rauc status --output-format=json 2>&1")
 
-    gateway.log("RAUC update install test passed — bundle installed to inactive slot, primary switched to B")
+    gateway.log("RAUC update install test passed — bundle installed to inactive slot, primary switched to B, and slot-local forensics remained readable")
   '';
 }

scripts/forensics-initrd-log.sh
@@ -1,0 +1,112 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+usage() {
+	cat <<'EOF'
+Usage:
+  forensics-initrd-log --event <event>
+EOF
+}
+
+EVENT=""
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+	--event)
+		EVENT="$2"
+		shift 2
+		;;
+	-h | --help)
+		usage
+		exit 0
+		;;
+	*)
+		printf 'Unknown argument: %s\n' "$1" >&2
+		usage >&2
+		exit 1
+		;;
+	esac
+done
+
+if [ -z "$EVENT" ]; then
+	usage >&2
+	exit 1
+fi
+
+slot="${ATOMIXOS_FORENSICS_SLOT:-}"
+lower_device="${ATOMIXOS_FORENSICS_LOWERDEV:-}"
+
+if [ -z "$slot" ] || [ -z "$lower_device" ]; then
+	for arg in $(</proc/cmdline); do
+		case "$arg" in
+		rauc.slot=boot.*)
+			if [ -z "$slot" ]; then
+				slot="${arg#rauc.slot=}"
+			fi
+			;;
+		atomixos.lowerdev=*)
+			if [ -z "$lower_device" ]; then
+				lower_device="${arg#atomixos.lowerdev=}"
+			fi
+			;;
+		esac
+	done
+fi
+
+if [ -z "$slot" ]; then
+	slot="boot.0"
+fi
+
+case "$slot" in
+boot.0)
+	boot_device="${ATOMIXOS_FORENSICS_BOOT0:-}"
+	;;
+boot.1)
+	boot_device="${ATOMIXOS_FORENSICS_BOOT1:-}"
+	;;
+*)
+	boot_device=""
+	;;
+esac
+
+mount_dir="${ATOMIXOS_FORENSICS_MOUNT:-/run/forensics-initrd}"
+mount_override=0
+if [ -n "${ATOMIXOS_FORENSICS_MOUNT:-}" ]; then
+	mount_override=1
+fi
+
+if [ "$mount_override" -eq 0 ] && [ -z "$boot_device" ]; then
+	printf 'forensics-initrd-log: missing boot device for slot %s\n' "$slot" >&2
+	exit 1
+fi
+
+mounted_here=0
+
+cleanup() {
+	if [ "$mounted_here" -eq 1 ]; then
+		umount "$mount_dir" >/dev/null 2>&1 || true
+	fi
+}
+
+trap cleanup EXIT INT TERM
+mkdir -p "$mount_dir"
+
+if [ "$mount_override" -eq 0 ] && ! findmnt "$mount_dir" >/dev/null 2>&1 && [ -n "$boot_device" ]; then
+	if ! mount -t vfat -o uid=0,gid=0,fmask=0133,dmask=0022 "$boot_device" "$mount_dir" >/dev/null 2>&1; then
+		printf 'forensics-initrd-log: failed to mount %s at %s\n' "$boot_device" "$mount_dir" >&2
+		exit 1
+	fi
+	mounted_here=1
+fi
+
+if [ "$EVENT" = "lowerdev-selected" ] && [ -z "$lower_device" ]; then
+	printf 'forensics-initrd-log: missing lower device for %s\n' "$EVENT" >&2
+	exit 1
+fi
+
+args=(--mount "$mount_dir" --slot "$slot" --stage initrd --event "$EVENT")
+if [ -n "$lower_device" ]; then
+	args+=(--device "$lower_device")
+fi
+
+forensic-log "${args[@]}"

scripts/os-verification.sh
@@ -5,10 +5,16 @@ set -euo pipefail
 
 # Dependencies (must be on PATH): rauc, jq, systemctl, ip
 
-SUSTAIN_DURATION=60 # 60 seconds
-CHECK_INTERVAL=5    # check every 5s during sustain
+SUSTAIN_DURATION="${ATOMIXOS_VERIFICATION_SUSTAIN_DURATION:-60}"
+CHECK_INTERVAL="${ATOMIXOS_VERIFICATION_CHECK_INTERVAL:-5}"
 
 log() { echo "[os-verification] $*"; }
+
+forensic() {
+	if command -v forensic-log >/dev/null 2>&1; then
+		forensic-log "$@" || true
+	fi
+}
 
 current_boot_slot() {
 	local arg
@@ -32,20 +38,42 @@ if [ -z "$SLOT_STATUS" ]; then
 	BOOT_SLOT="$(current_boot_slot || true)"
 	if [ -z "$BOOT_SLOT" ]; then
 		log "Could not determine boot slot from /proc/cmdline"
+		forensic --stage verify --event failed --reason missing-boot-slot
 		exit 1
 	fi
 	log "Assuming first boot, marking good: $BOOT_SLOT"
-	rauc status mark-good "$BOOT_SLOT" || true
-	exit 0
+	forensic --stage verify --event start --slot "$BOOT_SLOT"
+	forensic --stage rauc --event mark-good-start --slot "$BOOT_SLOT"
+	if rauc status mark-good "$BOOT_SLOT"; then
+		forensic --stage rauc --event mark-good-complete --slot "$BOOT_SLOT" --result ok
+		forensic --stage verify --event complete --slot "$BOOT_SLOT" --result ok
+		exit 0
+	fi
+	forensic --stage rauc --event mark-good-failed --slot "$BOOT_SLOT" --reason initial-boot
+	forensic --stage verify --event failed --slot "$BOOT_SLOT" --reason mark-good-failed
+	exit 1
 fi
 
-BOOT_GOOD=$(printf '%s\n' "$RAUC_STATUS_JSON" | jq -r '.slots[] | select(.state.booted == "booted") | .state.boot_status // "unknown"' 2>/dev/null || true)
+BOOT_GOOD=$(printf '%s\n' "$RAUC_STATUS_JSON" | jq -r '
+	.booted as $booted
+	| .slots[]
+	| to_entries[]
+	| select(.key == $booted)
+	| .value.boot_status // "unknown"
+' 2>/dev/null || true)
 if [ "$BOOT_GOOD" = "good" ]; then
 	log "Slot already marked good, nothing to do"
+	forensic --stage verify --event complete --result already-good
 	exit 0
 fi
 
 log "Slot is pending confirmation, running health checks..."
+BOOT_SLOT="$(current_boot_slot || true)"
+if [ -z "$BOOT_SLOT" ]; then
+	forensic --stage verify --event failed --reason missing-boot-slot
+	exit 1
+fi
+forensic --stage verify --event start --slot "$BOOT_SLOT"
 
 # ── Step 2: System health checks ──
 check_service() {
@@ -85,6 +113,7 @@ fi
 
 if [ "$SYSTEM_OK" != "true" ]; then
 	log "FAIL: System health checks failed"
+	forensic --stage verify --event failed --slot "$BOOT_SLOT" --reason system-health
 	exit 1
 fi
 
@@ -94,13 +123,14 @@ log "System health checks passed"
 log "Starting sustained health check (${SUSTAIN_DURATION}s)..."
 
 ELAPSED=0
-while [ $ELAPSED -lt $SUSTAIN_DURATION ]; do
-	sleep $CHECK_INTERVAL
+while [ "$ELAPSED" -lt "$SUSTAIN_DURATION" ]; do
+	sleep "$CHECK_INTERVAL"
 	ELAPSED=$((ELAPSED + CHECK_INTERVAL))
 
 	# Check system services still up
 	if ! systemctl is-active --quiet "dnsmasq.service" 2>/dev/null; then
 		log "FAIL: dnsmasq stopped during sustained check"
+		forensic --stage verify --event failed --slot "$BOOT_SLOT" --reason sustained-check
 		exit 1
 	fi
 done
@@ -111,9 +141,19 @@ log "Sustained health check passed (${SUSTAIN_DURATION}s)"
 BOOT_SLOT="$(current_boot_slot || true)"
 if [ -z "$BOOT_SLOT" ]; then
 	log "Could not determine boot slot from /proc/cmdline"
+	forensic --stage verify --event failed --reason missing-boot-slot
 	exit 1
 fi
 
 log "All checks passed, marking slot as good: $BOOT_SLOT"
-rauc status mark-good "$BOOT_SLOT"
-log "Slot committed successfully"
+forensic --stage rauc --event mark-good-start --slot "$BOOT_SLOT"
+if rauc status mark-good "$BOOT_SLOT"; then
+	forensic --stage rauc --event mark-good-complete --slot "$BOOT_SLOT" --result ok
+	forensic --stage verify --event complete --slot "$BOOT_SLOT" --result ok
+	log "Slot committed successfully"
+	exit 0
+fi
+
+forensic --stage rauc --event mark-good-failed --slot "$BOOT_SLOT" --reason health-check
+forensic --stage verify --event failed --slot "$BOOT_SLOT" --reason mark-good-failed
+exit 1