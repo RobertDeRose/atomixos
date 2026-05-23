RobertDeRose/AtomicNIx

feat(tests): custom bootloader backend for QEMU RAUC testing
Add a file-based bootloader backend script that simulates U-Boot
fw_setenv/fw_printenv for QEMU test environments. This enables
rauc status mark-good, slot switching, and boot state management
without requiring real U-Boot.

The backend is conditionally included in system.conf only when
bootloader=custom (QEMU), keeping the rock64 uboot config unchanged.

Verified: rauc service starts cleanly, rauc status lists all slots,
rauc status mark-good persists state via /var/lib/rauc/state.A.

modules/hardware-qemu.nix
@@ -48,4 +48,8 @@
     rootfs0 = "/dev/vdd";
     rootfs1 = "/dev/vde";
   };
+
+  # Use custom bootloader backend in QEMU — simulates U-Boot boot selection
+  # via plain files instead of fw_setenv/fw_printenv.
+  atomicnix.rauc.bootloader = "custom";
 }

RobertDeRose/atomixos

modules/hardware-qemu.nix
@@ -48,4 +48,8 @@
     rootfs0 = "/dev/vdd";
     rootfs1 = "/dev/vde";
   };
+
+  # Use custom bootloader backend in QEMU — simulates U-Boot boot selection
+  # via plain files instead of fw_setenv/fw_printenv.
+  atomixos.rauc.bootloader = "custom";
 }