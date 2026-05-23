RobertDeRose/AtomicNIx

fix: harden verification input fallbacks

nix/tests/first-boot-source-discovery.nix
@@ -106,6 +106,17 @@ nixos-lib.runTest {
     gateway.succeed("grep 'Configuration applied' /tmp/bootstrap-response.html")
     gateway.succeed("grep 'Download applied config.toml' /tmp/bootstrap-response.html")
     gateway.succeed("grep 'waiting for bootstrap address 172.20.30.1' /tmp/bootstrap-case.log")
+    gateway.succeed("rm -rf /tmp/bootstrap-invalid-lan-root /test-state && mkdir -p /tmp/bootstrap-invalid-lan-root /test-state")
+    gateway.succeed("rm -f /boot/config.toml /etc/atomicnix/fresh-flash /tmp/bootstrap-invalid-lan-sentinel")
+    gateway.succeed("rm -rf /test-usb/usb-a/* /test-usb/usb-b/*")
+    gateway.succeed("printf '{\"gateway_ip\": true}\n' >/tmp/bootstrap-invalid-lan-root/lan-settings.json")
+    gateway.succeed("ATOMICNIX_CONFIG_ROOT=/tmp/bootstrap-invalid-lan-root ATOMICNIX_FIRST_BOOT_SENTINEL=/tmp/bootstrap-invalid-lan-sentinel ATOMICNIX_BOOT_SLOT=boot.a ATOMICNIX_USB_SEARCH_DIRS='/test-usb' ATOMICNIX_BOOTSTRAP_HOST=127.0.0.1 ATOMICNIX_BOOTSTRAP_PORT=18081 PATH=/testbin:/run/current-system/sw/bin first-boot >/tmp/bootstrap-invalid-lan.log 2>&1 & echo $! >/tmp/bootstrap-invalid-lan.pid")
+    gateway.wait_until_succeeds("ss -tln | grep '127.0.0.1:18081'", timeout = 60)
+    gateway.succeed("curl -fsS -F config_file=@/tmp/config-template.toml http://127.0.0.1:18081/apply >/tmp/bootstrap-invalid-lan-response.html")
+    gateway.wait_until_succeeds("test -f /tmp/bootstrap-invalid-lan-root/config.toml", timeout = 60)
+    gateway.wait_until_succeeds("test -f /tmp/bootstrap-invalid-lan-sentinel", timeout = 60)
+    gateway.wait_until_succeeds("test ! -e /proc/$(cat /tmp/bootstrap-invalid-lan.pid)", timeout = 60)
+    gateway.succeed("grep 'Starting bootstrap web console on 127.0.0.1:18081' /tmp/bootstrap-invalid-lan.log")
     gateway.succeed("cat > /testbin/first-boot-provision-prodfail <<'EOF'\n#!/usr/bin/env bash\nset -euo pipefail\nif [ \"$1\" = serve ]; then\n  exit 1\nfi\nexec /run/current-system/sw/bin/first-boot-provision \"$@\"\nEOF\nchmod +x /testbin/first-boot-provision-prodfail")
     gateway.succeed("rm -rf /tmp/prod-missing-root /test-state && mkdir -p /tmp/prod-missing-root /test-state")
     gateway.succeed("rm -f /boot/config.toml /etc/atomicnix/fresh-flash")

nix/tests/rauc-confirm.nix
@@ -225,10 +225,20 @@ nixos-lib.runTest {
     gateway.fail("PATH=${raucStub}/bin:${pkgs.jq}/bin:${pkgs.systemd}/bin:${pkgs.iproute2}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:$PATH ATOMICNIX_VERIFICATION_SUSTAIN_DURATION=1 ATOMICNIX_VERIFICATION_CHECK_INTERVAL=1 ${verificationScript} >/tmp/os-verification-health-required.log 2>&1")
     gateway.succeed("grep 'Invalid required unit manifest: /data/config/health-required.json' /tmp/os-verification-health-required.log")
     gateway.succeed("test \"$(cat /var/lib/rauc/state.A)\" = pending")
+    gateway.succeed("printf '[\"traefik\", 7]\n' >/data/config/health-required.json")
+    gateway.fail("PATH=${raucStub}/bin:${pkgs.jq}/bin:${pkgs.systemd}/bin:${pkgs.iproute2}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:$PATH ATOMICNIX_VERIFICATION_SUSTAIN_DURATION=1 ATOMICNIX_VERIFICATION_CHECK_INTERVAL=1 ${verificationScript} >/tmp/os-verification-health-required-type.log 2>&1")
+    gateway.succeed("grep 'Invalid required unit manifest: /data/config/health-required.json' /tmp/os-verification-health-required-type.log")
+    gateway.succeed("test \"$(cat /var/lib/rauc/state.A)\" = pending")
     gateway.succeed("printf 'pending\n' > /var/lib/rauc/state.A")
     gateway.succeed("printf '[]\n' >/data/config/health-required.json")
     gateway.succeed("PATH=${raucStub}/bin:${pkgs.jq}/bin:${pkgs.systemd}/bin:${pkgs.iproute2}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:$PATH ATOMICNIX_VERIFICATION_SUSTAIN_DURATION=1 ATOMICNIX_VERIFICATION_CHECK_INTERVAL=1 ${verificationScript} >/tmp/os-verification-empty-required.log 2>&1")
     gateway.succeed("grep 'All checks passed, marking slot as good: boot.0' /tmp/os-verification-empty-required.log")
+    gateway.succeed("test \"$(cat /var/lib/rauc/state.A)\" = good")
+    gateway.succeed("printf 'pending\n' > /var/lib/rauc/state.A")
+    gateway.succeed("rm -f /data/config/health-required.json")
+    gateway.succeed("printf '{\"gateway_ip\": true}\n' >/data/config/lan-settings.json")
+    gateway.succeed("PATH=${raucStub}/bin:${pkgs.jq}/bin:${pkgs.systemd}/bin:${pkgs.iproute2}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:$PATH ATOMICNIX_VERIFICATION_SUSTAIN_DURATION=1 ATOMICNIX_VERIFICATION_CHECK_INTERVAL=1 ${verificationScript} >/tmp/os-verification-invalid-gateway-type.log 2>&1")
+    gateway.succeed("grep 'All checks passed, marking slot as good: boot.0' /tmp/os-verification-invalid-gateway-type.log")
     gateway.succeed("test \"$(cat /var/lib/rauc/state.A)\" = good")
 
     gateway.log("os-verification confirmation test passed — slot marked good after health checks")

RobertDeRose/atomixos

nix/tests/first-boot-source-discovery.nix
@@ -106,6 +106,17 @@ nixos-lib.runTest {
     gateway.succeed("grep 'Configuration applied' /tmp/bootstrap-response.html")
     gateway.succeed("grep 'Download applied config.toml' /tmp/bootstrap-response.html")
     gateway.succeed("grep 'waiting for bootstrap address 172.20.30.1' /tmp/bootstrap-case.log")
+    gateway.succeed("rm -rf /tmp/bootstrap-invalid-lan-root /test-state && mkdir -p /tmp/bootstrap-invalid-lan-root /test-state")
+    gateway.succeed("rm -f /boot/config.toml /etc/atomixos/fresh-flash /tmp/bootstrap-invalid-lan-sentinel")
+    gateway.succeed("rm -rf /test-usb/usb-a/* /test-usb/usb-b/*")
+    gateway.succeed("printf '{\"gateway_ip\": true}\n' >/tmp/bootstrap-invalid-lan-root/lan-settings.json")
+    gateway.succeed("ATOMIXOS_CONFIG_ROOT=/tmp/bootstrap-invalid-lan-root ATOMIXOS_FIRST_BOOT_SENTINEL=/tmp/bootstrap-invalid-lan-sentinel ATOMIXOS_BOOT_SLOT=boot.a ATOMIXOS_USB_SEARCH_DIRS='/test-usb' ATOMIXOS_BOOTSTRAP_HOST=127.0.0.1 ATOMIXOS_BOOTSTRAP_PORT=18081 PATH=/testbin:/run/current-system/sw/bin first-boot >/tmp/bootstrap-invalid-lan.log 2>&1 & echo $! >/tmp/bootstrap-invalid-lan.pid")
+    gateway.wait_until_succeeds("ss -tln | grep '127.0.0.1:18081'", timeout = 60)
+    gateway.succeed("curl -fsS -F config_file=@/tmp/config-template.toml http://127.0.0.1:18081/apply >/tmp/bootstrap-invalid-lan-response.html")
+    gateway.wait_until_succeeds("test -f /tmp/bootstrap-invalid-lan-root/config.toml", timeout = 60)
+    gateway.wait_until_succeeds("test -f /tmp/bootstrap-invalid-lan-sentinel", timeout = 60)
+    gateway.wait_until_succeeds("test ! -e /proc/$(cat /tmp/bootstrap-invalid-lan.pid)", timeout = 60)
+    gateway.succeed("grep 'Starting bootstrap web console on 127.0.0.1:18081' /tmp/bootstrap-invalid-lan.log")
     gateway.succeed("cat > /testbin/first-boot-provision-prodfail <<'EOF'\n#!/usr/bin/env bash\nset -euo pipefail\nif [ \"$1\" = serve ]; then\n  exit 1\nfi\nexec /run/current-system/sw/bin/first-boot-provision \"$@\"\nEOF\nchmod +x /testbin/first-boot-provision-prodfail")
     gateway.succeed("rm -rf /tmp/prod-missing-root /test-state && mkdir -p /tmp/prod-missing-root /test-state")
     gateway.succeed("rm -f /boot/config.toml /etc/atomixos/fresh-flash")

nix/tests/rauc-confirm.nix
@@ -225,10 +225,20 @@ nixos-lib.runTest {
     gateway.fail("PATH=${raucStub}/bin:${pkgs.jq}/bin:${pkgs.systemd}/bin:${pkgs.iproute2}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:$PATH ATOMIXOS_VERIFICATION_SUSTAIN_DURATION=1 ATOMIXOS_VERIFICATION_CHECK_INTERVAL=1 ${verificationScript} >/tmp/os-verification-health-required.log 2>&1")
     gateway.succeed("grep 'Invalid required unit manifest: /data/config/health-required.json' /tmp/os-verification-health-required.log")
     gateway.succeed("test \"$(cat /var/lib/rauc/state.A)\" = pending")
+    gateway.succeed("printf '[\"traefik\", 7]\n' >/data/config/health-required.json")
+    gateway.fail("PATH=${raucStub}/bin:${pkgs.jq}/bin:${pkgs.systemd}/bin:${pkgs.iproute2}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:$PATH ATOMIXOS_VERIFICATION_SUSTAIN_DURATION=1 ATOMIXOS_VERIFICATION_CHECK_INTERVAL=1 ${verificationScript} >/tmp/os-verification-health-required-type.log 2>&1")
+    gateway.succeed("grep 'Invalid required unit manifest: /data/config/health-required.json' /tmp/os-verification-health-required-type.log")
+    gateway.succeed("test \"$(cat /var/lib/rauc/state.A)\" = pending")
     gateway.succeed("printf 'pending\n' > /var/lib/rauc/state.A")
     gateway.succeed("printf '[]\n' >/data/config/health-required.json")
     gateway.succeed("PATH=${raucStub}/bin:${pkgs.jq}/bin:${pkgs.systemd}/bin:${pkgs.iproute2}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:$PATH ATOMIXOS_VERIFICATION_SUSTAIN_DURATION=1 ATOMIXOS_VERIFICATION_CHECK_INTERVAL=1 ${verificationScript} >/tmp/os-verification-empty-required.log 2>&1")
     gateway.succeed("grep 'All checks passed, marking slot as good: boot.0' /tmp/os-verification-empty-required.log")
+    gateway.succeed("test \"$(cat /var/lib/rauc/state.A)\" = good")
+    gateway.succeed("printf 'pending\n' > /var/lib/rauc/state.A")
+    gateway.succeed("rm -f /data/config/health-required.json")
+    gateway.succeed("printf '{\"gateway_ip\": true}\n' >/data/config/lan-settings.json")
+    gateway.succeed("PATH=${raucStub}/bin:${pkgs.jq}/bin:${pkgs.systemd}/bin:${pkgs.iproute2}/bin:${pkgs.coreutils}/bin:${pkgs.gnugrep}/bin:$PATH ATOMIXOS_VERIFICATION_SUSTAIN_DURATION=1 ATOMIXOS_VERIFICATION_CHECK_INTERVAL=1 ${verificationScript} >/tmp/os-verification-invalid-gateway-type.log 2>&1")
+    gateway.succeed("grep 'All checks passed, marking slot as good: boot.0' /tmp/os-verification-invalid-gateway-type.log")
     gateway.succeed("test \"$(cat /var/lib/rauc/state.A)\" = good")
 
     gateway.log("os-verification confirmation test passed — slot marked good after health checks")